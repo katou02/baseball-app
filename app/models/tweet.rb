0aef945eac53781b8ab3fcf864e2317128ee05e2
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-03-29 19:21:34 +0900

    バリデーション

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ApplicationRecord
   validates :title_info,length: {maximum: 30}
+  validates :school_a,:school_b,:school_a_score,:school_b_score,:text ,presence: true
+  validates :school_a_score,:school_b_score, numericality: true
 end
