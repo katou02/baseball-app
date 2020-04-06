ce68df697979c864b54c66c7aac86acefb26162c
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-04-06 16:50:25 +0900

    色々

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+  # def show
+  #   @nickname = current_user.nickname
+  #   @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "tweets#index"
   resources :tweets
+  get 'users/:id' => 'users#show'
 end
