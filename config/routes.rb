78db43100253cb20b1b9350aa3010515f49f5507
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-05-17 12:04:45 +0900

    コメントフォーム

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "tweets#index"
   resources :tweets
-  get 'users/:id' => 'users#show'
+  resources :show,only:[:show]
+  # get 'users/:id' => 'users#show'
 end
