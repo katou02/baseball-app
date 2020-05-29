64f03bdd266610dfc18216493caf92f3e470de00
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-05-29 14:22:56 +0900

    up

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'baseball-app'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/baseball-app'
 end
