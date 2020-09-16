a059ccfed09e6ab1ab79b51fb71926f8a989b260
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-09-16 18:25:00 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'tweets#index'
   devise_for :users
   resources :tweets do
     resources :comments,only:[:create,:destroy]
@@ -8,10 +9,6 @@ Rails.application.routes.draw do
   end
   resources :tournaments,only:[:new,:create]
   resources :show,only:[:show]
-  resources :top,only:[:index] do
-  collection do
-    get :choose
-  end
-end
+  resources :top,only:[:index]
   # get 'users/:id' => 'users#show'
 end
