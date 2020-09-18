61f8b3d9a7781c8d2e56a7fdbc5e5b3376cc0b19
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-09-18 14:17:38 +0900

    スクールr c v

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,23 @@
+class SchoolsController < ApplicationController
+  def new
+    @school = School.new
+  end
+
+  def create
+    @school = School.create(name: name_params[:name])
+    if @school.save
+      redirect_to action: :new
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @tweets = Tweet.where(tournament_id: params[:id]).includes(:user).page(params[:page]).per(5).order("created_at DESC")
+  end
+
+  private
+  def name_params
+    params.require(:school).permit(:name)
+  end
+end
