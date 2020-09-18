6ab7ecb83d1b2e9ba1a6181484a7b6a6ca6e6ca6
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-09-18 14:17:38 +0900

    スクールr c v

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :tournaments,only:[:new,:create,:show]
   resources :show,only:[:show]
   resources :top,only:[:index]
+  resources :schools,only:[:new,:create,:show]
   # get 'users/:id' => 'users#show'
 end
