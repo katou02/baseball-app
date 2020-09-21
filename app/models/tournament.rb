35d118ae092fa0219aa01246d0a0c01fe14d6df7
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-09-21 16:10:28 +0900

    カテゴリーテーブル

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,6 +1,6 @@
 class Tournament < ApplicationRecord
   has_many :tournament_schools
   has_many :schools,  through: :tournament_schools
-
+  belongs_to :category
   validates :name,presence: true,uniqueness: true
 end
