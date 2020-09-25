406ba2b24460ba231d9f130207acc64a07263ef2
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-09-25 17:09:39 +0900

    スクール　トーナメント　モデル削除

--- app/models/school.rb
+++
@@ -1,6 +0,0 @@
-class School < ApplicationRecord
-  has_many :tournament_schools
-  has_many :tournaments, through: :tournament_schools
-
-  validates :name,presence: true
-end
--- db/migrate/20200918055548_create_tournament_schools.rb
+++
@@ -1,9 +0,0 @@
-class CreateTournamentSchools < ActiveRecord::Migration[5.2]
-  def change
-    create_table :tournament_schools do |t|
-      t.references :school, foreign_key: true
-      t.references :tournament, foreign_key: true
-      t.timestamps
-    end
-  end
-end
