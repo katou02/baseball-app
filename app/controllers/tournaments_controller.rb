e388a6462f9a437b7ea2d37cd67c213d667d9ff5
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-09-25 17:45:13 +0900

    大会別 記事閲覧

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,2 +1,5 @@
 class TournamentsController < ApplicationController
+  def show
+    @tweets = Tweet.where(tournament_id: params[:id]).includes(:user).page(params[:page]).per(5).order("created_at DESC")
+  end
 end
