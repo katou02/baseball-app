fb78bb98c79ea5979ab6fbd605cb51bb17ac3898
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-09-25 17:57:10 +0900

    search json

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -35,10 +35,13 @@ class TweetsController < ApplicationController
 
   def search
     @tweets = Tweet.search(params[:keyword])
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
-    @tweets = Tweet.find(params[:id])
   end
 
   def update
