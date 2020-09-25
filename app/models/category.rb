0bc4ec818c49e2719b1747770b7b074f4d64b711
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-09-25 14:34:25 +0900

    カラム変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
   has_many :tweets
   has_ancestry
 
-  has_many :school_as_tweets,class_name: 'Tweet',  foreign_key: 'school_as_id'
-  has_many :school_bs_tweets,class_name: 'Tweet',  foreign_key: 'school_bs_id'
+  has_many :school_a_tweets,class_name: 'Tweet',  foreign_key: 'school_a_id'
+  has_many :school_b_tweets,class_name: 'Tweet',  foreign_key: 'school_b_id'
 end
