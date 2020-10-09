f2f53039602ec95e36b7b5c49b2491d091c8c467
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-10 01:11:14 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,12 +9,11 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'baseball-app'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/baseball-app'
-  end
 end
