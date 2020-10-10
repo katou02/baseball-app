d3f2c0e869010e0032c7cac4f49625cdf6593890
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-10 11:30:39 +0900

    大会　紐付け修正

--- db/migrate/20200322061505_create_tweets.rb
+++ db/migrate/20200322061505_create_tweets.rb
@@ -6,10 +6,8 @@ class CreateTweets < ActiveRecord::Migration[5.2]
       t.text   :image
       t.integer :school_a_score
       t.integer :school_b_score
-      t.integer :tournament_id
+      # t.integer :tournament_id
       t.timestamps null: true
-      # t.references :school_a, foreign_key: { to_table: :categories }, null: false
-      # t.references :school_b, foreign_key: { to_table: :categories }, null: false
     end
   end
 end
