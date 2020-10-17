55eac2d63ef98110a3f8cb9e36969abb4a7b9f5c
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-17 22:41:49 +0900

    new バリデーション

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -3,4 +3,5 @@ class Analysis < ApplicationRecord
   belongs_to :school,class_name: 'Category', foreign_key: 'school_id'
   belongs_to :user
   has_many :categories
+  validates :text,:attack,:defensive,:pitcher,:comprehensive,:school,:tournament,presence: true
 end
