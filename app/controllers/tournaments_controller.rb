af4be881761e7d4c633568495cf44b2ea8a45946
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-18 14:17:41 +0900

    平均評価　改修

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -4,6 +4,7 @@ class TournamentsController < ApplicationController
   end
 
   def watch
+    @analysis = Category.find(params[:id])
     @analyses = Analysis.where(tournament_id: params[:id]).includes(:user).page(params[:page]).per(10).order("created_at DESC")
   end
   
