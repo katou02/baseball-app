4203b33b25f6e86b7a7b002345378c946ba0f395
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-18 19:28:00 +0900

    モデル関連

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,2 +1,6 @@
 class Forecast < ApplicationRecord
+  has_many :categories
+  belongs_to :win_school,class_name: 'Category', foreign_key: 'win_school_id'
+  belongs_to :lose_school,class_name: 'Category', foreign_key: 'lose_school_id'
+  belongs_to :tournament,class_name: 'Category', foreign_key: 'tournament_id'
 end
