67d3194828b9b3ecc07f8a3063aa481481b8c45a
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-19 21:14:07 +0900

    投稿画面

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,2 +1,21 @@
 class ForecastsController < ApplicationController
+  before_action :set_category, only: [:new]
+
+  def index
+  end
+
+  def new
+    @forecast = Forecast.new
+    respond_to do |format|
+      format.html
+      format.json do
+      @category_children = Category.find(params[:tournament_id]).children
+      end
+    end
+  end
+
+  private
+  def set_category
+    @category_parent_array = Category.where(ancestry: nil)
+  end
 end
