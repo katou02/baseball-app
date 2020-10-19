b01089877a79b1d620b010983e4440c224ebdedd
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-19 23:48:25 +0900

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :tweets
   has_many :analyses
+  has_many :forecasts
   has_ancestry
   has_many :school_a_tweets,class_name: 'Tweet',  foreign_key: 'school_a_id'
   has_many :school_b_tweets,class_name: 'Tweet',  foreign_key: 'school_b_id'
@@ -9,5 +10,5 @@ class Category < ApplicationRecord
   has_many :tournament_analyses,class_name: 'Analysis',  foreign_key: 'tournament_id'
   has_many :win_school_forecasts,class_name: 'Forecast',  foreign_key: 'win_school_id'
   has_many :lose_school_forecasts,class_name: 'Forecast',  foreign_key: 'lose_school_id'
-  has_many :tournament_analyses,class_name: 'Forecast',  foreign_key: 'tournament_id'
+  has_many :tournament_forecasts,class_name: 'Forecast',  foreign_key: 'tournament_id'
 end
