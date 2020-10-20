4a3def6f07482f47012745598850470b21fb0747
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-21 00:05:16 +0900

    投稿　バリデーション

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -4,4 +4,5 @@ class Forecast < ApplicationRecord
   belongs_to :win_school,class_name: 'Category', foreign_key: 'win_school_id'
   belongs_to :lose_school,class_name: 'Category', foreign_key: 'lose_school_id'
   belongs_to :tournament,class_name: 'Category', foreign_key: 'tournament_id'
+  validates :win_school,:lose_school,:text,:tournament,presence: true
 end
