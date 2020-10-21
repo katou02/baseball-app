c95cdae498eae54fbd175f388c615ed8a45d8dfa
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-21 23:20:14 +0900

    Update administrator_controller.rb

--- app/controllers/admin/administrator_controller.rb
+++ app/controllers/admin/administrator_controller.rb
@@ -1,2 +1,8 @@
 class Admin::AdministratorController < ApplicationController
+  before_action :if_not_admin
+
+  private
+  def if_not_admin
+    redirect_to root_path unless current_user.admin?
+  end
 end
