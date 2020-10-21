c0aa23a535be623dd0cce6dc0af6072fe81480b4
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-21 23:29:42 +0900

    削除

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,5 @@ Rails.application.routes.draw do
   resources :analyses,only:[:index,:new,:create,:destroy,:edit,:update]
   resources :forecasts,only:[:index,:new,:create,:destroy,:edit,:update]
   resources :tops,only:[:index]
+  resources :administrator,only:[:index]
 end
