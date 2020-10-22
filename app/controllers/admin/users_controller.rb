c5c0ed498928692046cdc6e06847c7302918d5da
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-22 20:17:08 +0900

    ユーザー削除機能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,20 @@
 class Admin::UsersController < ApplicationController
+  before_action :admin_user
+
+  def index
+  end
+  
+  def users_show
+    @users = User.all.order(created_at: :desc)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  private
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
