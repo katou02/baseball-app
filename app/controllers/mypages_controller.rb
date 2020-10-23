ccb5fac23400c2fad861a377530446da81663148
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-23 18:57:58 +0900

    作成

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,2 +1,33 @@
 class MypagesController < ApplicationController
+  def show
+    @nickname = current_user.nickname
+    user = User.find(params[:id])
+  end
+
+  def new
+    @mypage = Mypage.new
+  end
+
+  def create
+    @mypage = Mypage.create(mypage_params)
+    if @mypage.save
+      redirect_to "/mypages/#{current_user.id}"
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @mypage = Mypage.find(params[:id])
+  end
+
+  def update
+    @mypage.update(mypage_params) if @mypage.user_id == current_user.id || current_user.admin
+    redirect_to action: :show
+  end
+
+  private
+    def mypage_params
+      params.require(:mypage).permit(:text).merge(user_id: current_user.id)
+    end
 end
