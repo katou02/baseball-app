d61e192180ad937d0e5c3d6789e5cd7956b5d64c
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-23 17:18:46 +0900

    mypage model

--- db/migrate/20201023081120_create_mypages.rb
+++ db/migrate/20201023081120_create_mypages.rb
@@ -1,7 +1,8 @@
 class CreateMypages < ActiveRecord::Migration[5.2]
   def change
     create_table :mypages do |t|
-
+      t.text :text
+      t.integer :user_id
       t.timestamps
     end
   end
