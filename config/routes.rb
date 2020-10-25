edfb0ddf4b765b362aaa1c3e186cd33500c429d1
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-25 10:47:13 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -32,4 +32,10 @@ Rails.application.routes.draw do
       end
     end
   end
+  resources :users do
+    member do
+      get :following,:followers
+    end
+  end
+  resources :relationships, only: [:create,:destroy]
 end
