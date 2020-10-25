001efbdcdb659499f4ae17bcae55c854a7c249c6
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-25 10:47:11 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def following
+    @user = User.find(params[:id])
+    @users = @user.followings
+    render "show_follow"
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render "show_follower"
+  end
 end
