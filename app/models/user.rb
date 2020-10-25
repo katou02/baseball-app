95e52c6dc644e9af16277a2c5a2a8608d5a4d2e3
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-25 11:07:23 +0900

    フォロー　フォロワー　一覧

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,10 @@ class User < ApplicationRecord
   has_many :mypages
   has_many :likes,dependent: :destroy
   has_many :liked_tweets,through: :likes,source: :tweet
-  has_many :following_relationships,foreign_key: "follower_id",class_name: "Relationship",dependent: :destroy
-  has_many :followings,through: :following_relationships
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
 
   def following?(other_user)
     following_relationships.find_by(following_id: other_user.id)
