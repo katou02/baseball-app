ff9f273be759af2e1add18ec35595b8c8169c097
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-25 14:09:49 +0900

    フォロー、フォロー外した後

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:following_id])
     current_user.follow!(@user)
-    redirect_to @user
+    redirect_to "/mypages/#{@user.id}"
   end
 
   def destroy
     @user = Relationship.find(params[:id]).following_id
     current_user.unfollow!(@user)
-    redirect_to @user
+    redirect_to "/mypages/#{@user}"
   end
 end
