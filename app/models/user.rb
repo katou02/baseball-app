e5845ee8f03ebca4635dcab44801661ca964dc45
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-26 18:02:36 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,15 @@ class User < ApplicationRecord
   def already_liked?(tweet)
     self.likes.exists?(tweet_id: tweet.id)
   end
+
+  def create_notification_follow!(current_user)
+    temp = Notification.where(["visiter_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: id,
+        action: 'follow'
+      )
+      notification.save if notification.valid?
+    end
+  end
 end
