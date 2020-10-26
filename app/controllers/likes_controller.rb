a7aebaace88d26a67f670173f704ef729987a0ef
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-26 18:07:24 +0900

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,13 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.create(tweet_id: params[:tweet_id])
     redirect_back(fallback_location: root_path)
+
+    @tweet = Tweet.find(params[:tweet_id])
+    @tweet.create_notification_by(current_uset)
+    respond_to do |format|
+      format.html {redirect_to request.referrer}
+      format.js
+    end
   end
 
   def destroy
