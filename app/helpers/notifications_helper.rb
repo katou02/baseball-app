49ad30a5eb26049ce8cf0335074e71520ddba0ba
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-26 18:17:37 +0900

    Update notifications_helper.rb

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,17 @@
 module NotificationsHelper
+  def notification_form(notification)
+    @visiter = notification.visiter
+    @comment = nil
+    your_item = link_to 'あなたの投稿', users_tweet_path(notification), style:"font-weight: bold;"
+    @visiter_comment = notification.comment_id
+    case notification.action
+      when "follow" then
+        tag.a(notification.visiter.name, href:users_user_path(@visiter), style:"font-weight: bold;")+"があなたをフォローしました"
+      when "like" then
+        tag.a(notification.visiter.name, href:users_user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:users_tweet_path(notification.tweet_id), style:"font-weight: bold;")+"にいいねしました"
+      when "comment" then
+          @comment = Comment.find_by(id: @visiter_comment)&.content
+          tag.a(@visiter.name, href:users_user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:users_tweet_path(notification.tweet_id), style:"font-weight: bold;")+"にコメントしました"
+    end
+  end
 end
