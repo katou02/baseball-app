1b440e208393121fe3bd82522796a16351539c38
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-26 17:30:38 +0900

    Update notification.rb

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,6 @@
 class Notification < ApplicationRecord
+  belongs_to :tweet,optional: true
+  belongs_to :comment,optional: true
+  belongs_to :visiter,class_name: "User",foreign_key: "visiter_id", optional: true
+  belongs_to :visited,class_name: "User",foreign_key: "visited_id", optional: true
 end
