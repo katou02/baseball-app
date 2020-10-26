878d55ba6133fc6a7397a5d2a8f89476e8e34484
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-26 17:59:55 +0900

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,27 @@
 class Comment < ApplicationRecord
   belongs_to :tweet
   belongs_to :user
+
+  def create_notification_comment!(current_user, comment_id)
+    temp_ids = Comment.select(:user_id).where(tweet_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_user,comment_id,temp_id["user_id"])
+    end
+    save_notification_comment!(current_user, comment_id, user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment!(current_user, comment_id, visited_id)
+    notification = current_user.active_notifications.new(
+      item_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: 'comment'
+
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    )
+    
+  end
 end
