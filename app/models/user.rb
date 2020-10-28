6b32a3dff8b78aa27f20737dcbd0968c4ee486a9
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-28 16:14:34 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_many :mypages
   has_many :likes,dependent: :destroy
   has_many :liked_tweets,through: :likes,source: :tweet
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
   has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
   has_many :followings, through: :following_relationships
   has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
