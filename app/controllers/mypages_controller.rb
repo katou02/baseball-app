1a454ddc0800d27c0d4296b328cdcb005d2c1f9a
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-28 16:26:12 +0900

    Update mypages_controller.rb

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,9 +1,30 @@
 class MypagesController < ApplicationController
   before_action :search_mypage,except:[:new,:create,:update]
+  before_action :authenticate_user!
 
   def show
     @user = User.find(params[:id])
     @mypages = @user.mypages
+
+    @myEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    if @user.id == current_user.id
+      @msg ="他のユーザーとDMしてみよう！"
+    else
+      @myEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+
+      if @isRoom != true
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def new
