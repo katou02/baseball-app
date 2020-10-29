8bb1368a00635e9e337b8fde83fc8208132d812b
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-29 18:25:49 +0900

    dm 通知　モデル

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,4 +4,6 @@ class Notification < ApplicationRecord
   belongs_to :comment,optional: true
   belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id', optional: true
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
+  belongs_to :room, optional: true
+  belongs_to :message, optional: true
 end
