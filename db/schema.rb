734043cb95701aa4ae7c8bcea2c5cf1a5eb8a96b
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-29 20:31:10 +0900

    commentanalysisモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_071015) do
+ActiveRecord::Schema.define(version: 2020_10_29_112307) do
 
   create_table "analyses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 2020_10_28_071015) do
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comment_analyses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "analysis_id"
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ test/models/comment_analysis_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CommentAnalysisTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
