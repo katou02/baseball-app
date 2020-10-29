5f0b5c5a67e4b31209dc20c95636fe410be7fd12
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-29 20:36:04 +0900

    commentanalysis アソシエーション

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -2,6 +2,7 @@ class Analysis < ApplicationRecord
   belongs_to :tournament,class_name: 'Category', foreign_key: 'tournament_id'
   belongs_to :school,class_name: 'Category', foreign_key: 'school_id'
   belongs_to :user
+  has_many :comment_analyses
   has_many :categories
   validates :text,:attack,:defensive,:pitcher,:comprehensive,:school,:tournament,presence: true
 end
