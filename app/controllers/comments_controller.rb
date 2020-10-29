5fecff4089bf149f2aa643092837b7928925a29c
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-29 15:14:49 +0900

    詳細ページ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,8 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
+    comment = Comment.create(comment_params)
     redirect_to "/tweets/#{comment.tweet.id}"
     @tweet = comment.tweet
-
     @tweet.create_notification_comment!(current_user, comment.id)
   end
 
@@ -15,9 +14,8 @@ class CommentsController < ApplicationController
     end
   end
 
-
   private
   def comment_params
-    params.permit(:text, :tweet_id)
+    params.permit(:text, :tweet_id).merge(user_id: current_user.id)
   end
 end
