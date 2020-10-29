7af94c0d7d0cf052ace303d8d1f872edce361fa8
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-29 15:38:20 +0900

    analysis ルーティング　編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'tops#index'
   devise_for :users
-  resources :analyses,only:[:index,:new,:create,:destroy,:edit,:update,:show]
+  resources :analyses
   resources :tweets do
     collection do
       get :search
