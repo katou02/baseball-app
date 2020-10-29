3ed9f0f8b372d5de8404f843e09f21ceb918e21e
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-29 20:45:37 +0900

    CommentAnalysesコントローラー作成

---
+++ app/helpers/comment_analyses_helper.rb
@@ -0,0 +1,2 @@
+module CommentAnalysesHelper
+end
---
+++ test/controllers/comment_analyses_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CommentAnalysesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
