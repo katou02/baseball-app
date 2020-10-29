230a191aa00bedb64a7d2e82b86bd740488b7b49
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-29 18:25:57 +0900

    通知メッセージ

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,18 +2,22 @@ module NotificationsHelper
   def notification_form(notification)
     @visiter = notification.visiter
     @comment = nil
+    @message = nil
     your_item = link_to 'あなたの投稿', tweets_path(notification), style:"font-weight: bold;"
     @visiter_comment = notification.comment_id
+    @message_comment = notification.message_id
     case notification.action
       when "follow" then
         tag.a(notification.visiter.nickname, href:mypage_path(@visiter), style:"font-weight: bold;")+"があなたをフォローしました"
       when "like" then
         tag.a(notification.visiter.nickname, href:mypage_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:tweet_path(notification.tweet_id), style:"font-weight: bold;")+"にいいねしました"
       when "comment" then
-          @comment = Comment.find_by(id: @visiter_comment)
-          tag.a(@visiter.nickname, href:mypage_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:tweet_path(notification.tweet_id), style:"font-weight: bold;")+"にコメントしました"
+        @comment = Comment.find_by(id: @visiter_comment)
+        tag.a(@visiter.nickname, href:mypage_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:tweet_path(notification.tweet_id), style:"font-weight: bold;")+"にコメントしました"
+      when "dm" then
+        @message = Message.find_by(id: @visiter_message)
+        tag.a(@visiter.nickname, href:mypage_path(@visiter), style:"font-weight: bold;")+"があなたに"+tag.a('ダイレクトメッセージ', href:room_path(notification.room_id), style:"font-weight: bold;")+"を送りました"
     end
-
   end
   
   def unchecked_notifications
