1d46b66f00818753b8a0e1ae5edeaab5384e3c3a
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-30 14:38:43 +0900

    テーブル作成

--- db/migrate/20201030052859_create_comment_forecasts.rb
+++ db/migrate/20201030052859_create_comment_forecasts.rb
@@ -1,7 +1,9 @@
 class CreateCommentForecasts < ActiveRecord::Migration[5.2]
   def change
     create_table :comment_forecasts do |t|
-
+      t.integer :user_id
+      t.integer :forecast_id
+      t.text :text
       t.timestamps
     end
   end
