734969060d4f599567e0ac1505268565cbb07b9b
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-30 16:58:08 +0900

    通知追加

--- db/schema.rb
+++ db/schema.rb
@@ -111,9 +111,13 @@ ActiveRecord::Schema.define(version: 2020_10_30_052859) do
     t.integer "visiter_id"
     t.integer "visited_id"
     t.integer "tweet_id"
+    t.integer "analysis_id"
+    t.integer "forecast_id"
     t.integer "room_id"
     t.integer "message_id"
     t.integer "comment_id"
+    t.integer "comment_analysis_id"
+    t.integer "comment_forecast_id"
     t.string "action"
     t.boolean "checked", default: false, null: false
     t.datetime "created_at", null: false
