0c3538e6e77e942120aee389784ab129e0ae3c8b
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-30 15:23:13 +0900

    Update comment_forecasts_controller.rb

--- app/controllers/comment_forecasts_controller.rb
+++ app/controllers/comment_forecasts_controller.rb
@@ -1,2 +1,20 @@
 class CommentForecastsController < ApplicationController
+  def create
+    comment = CommentForecast.create(comment_params)
+    redirect_to "/forecasts/#{comment.forecast.id}"
+    @analysis = comment.forecast
+  end
+
+  def destroy
+    comment = CommentForecast.find(params[:id])
+    if comment.user_id = current_user.id
+      comment.destroy
+      redirect_to "/forecasts/#{comment.forecast.id}"
+    end
+  end
+
+  private
+  def comment_params
+    params.permit(:text, :forecast_id).merge(user_id: current_user.id)
+  end
 end
