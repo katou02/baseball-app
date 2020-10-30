ae998db1f63d1b8b349e3be8392dd643feeb261e
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-30 14:38:43 +0900

    テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_112307) do
+ActiveRecord::Schema.define(version: 2020_10_30_052859) do
 
   create_table "analyses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2020_10_29_112307) do
   create_table "comment_analyses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "analysis_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comment_forecasts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "forecast_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
