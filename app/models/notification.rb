af12e94fbbfcf853091a249c4a1e612ee72fc603
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-10-30 17:00:45 +0900

    通知　アソシエーション

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,11 @@
 class Notification < ApplicationRecord
   default_scope->{order(created_at: :desc)}
   belongs_to :tweet,optional: true
+  belongs_to :analysis,optional: true
+  belongs_to :forecast,optional: true
   belongs_to :comment,optional: true
+  belongs_to :comment_analysis,optional: true
+  belongs_to :comment_forecast,optional: true
   belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id', optional: true
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
   belongs_to :room, optional: true
