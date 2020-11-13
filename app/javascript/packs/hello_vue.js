7bc67142a2c31c8a0ec95db3efd22043a3694970
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-11-13 20:12:33 +0900

    vue 準備3

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -6,11 +6,13 @@
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
 import Vue from 'vue'
-import App from '../app.vue'
+// import App from '../app.vue'
+import Top from '../top.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    render: h => h(App)
+    // render: h => h(App),
+    render: h => h(Top)
   }).$mount()
   document.body.appendChild(app.$el)
 
