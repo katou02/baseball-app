e9117705548e933cf3b66534dbc46511a5b18649
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-11-30 15:02:44 +0900

    tweet ターボリンクス

--- app/javascript/packs/tweet.js
+++ app/javascript/packs/tweet.js
@@ -1,3 +1,4 @@
+document.addEventListener("turbolinks:load", function() {
 window.onload=function(){
   new Vue({
     el: '#app',
@@ -5,13 +6,17 @@ window.onload=function(){
       current_slide: 0,
       slides: [
         {img: "/assets/81573810.jpeg"},
-        {img: "/assets/ball.jpg"}
+        {img: "/assets/ball.jpg"},
+        {img: "/assets/thumb_ground.jpg"},
+        {img: "/assets/thumb_front.jpg"},
+        {img: "/assets/mykosien.JPG"}
       ],
     },
     mounted() {
       setInterval(() => {
         this.current_slide = this.current_slide < this.slides.length -1 ? this.current_slide +1 : 0
-      }, 5000)
+      }, 4000)
     }
   })
-}
+};
+});
