740db0f3cb5b07b402a7d47e84bd2738e20df3b1
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-10 22:43:09 +0900

    roundカラム　DB保存

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -60,10 +60,10 @@ class ForecastsController < ApplicationController
   end
 
   def forecast_params
-    params.require(:forecast).permit(:text,:win_school_id,:lose_school_id,:tournament_id,:probability).merge(user_id: current_user.id)
+    params.require(:forecast).permit(:text,:round,:win_school_id,:lose_school_id,:tournament_id,:probability).merge(user_id: current_user.id)
   end
 
   def update_params
-    params.require(:forecast).permit(:text,:win_school_id,:lose_school_id,:tournament_id,:probability)
+    params.require(:forecast).permit(:text,:round,:win_school_id,:lose_school_id,:tournament_id,:probability)
   end
 end
