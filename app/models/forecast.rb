a58544bfd0f6dffd241dc525f1b3981bd5331533
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-10 22:26:26 +0900

    Update forecast.rb

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -6,7 +6,7 @@ class Forecast < ApplicationRecord
   belongs_to :win_school,class_name: 'Category', foreign_key: 'win_school_id'
   belongs_to :lose_school,class_name: 'Category', foreign_key: 'lose_school_id'
   belongs_to :tournament,class_name: 'Category', foreign_key: 'tournament_id'
-  validates :win_school,:lose_school,:text,:tournament,:probability,presence: true
+  validates :win_school,:lose_school,:text,:round,:tournament,:probability,presence: true
 
   def create_notification_comment_forecast!(current_user, comment_forecast_id)
     temp_ids = CommentForecast.select(:user_id).where(forecast_id: id).where.not(user_id: current_user.id).distinct
