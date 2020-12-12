e498667e41f0a5270f02b4127aefbe3d7f8a4e39
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-12 16:22:32 +0900

    championsテーブル作成

--- db/migrate/20201212071407_create_champions.rb
+++ db/migrate/20201212071407_create_champions.rb
@@ -1,7 +1,9 @@
 class CreateChampions < ActiveRecord::Migration[5.2]
   def change
     create_table :champions do |t|
-
+      t.integer :user_id
+      t.references :champion_school, foreign_key: { to_table: :categories }, null: false
+      t.references :tournament, foreign_key: { to_table: :categories }, null: false
       t.timestamps
     end
   end
