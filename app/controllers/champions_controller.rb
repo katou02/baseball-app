4859bdc527bbfc01320ba7430dfc6cae951a9c85
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-12 17:37:11 +0900

    投稿機能　js起動

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -1,4 +1,27 @@
 class ChampionsController < ApplicationController
+  before_action :set_category, only:[:new,:create]
+
+  def new
+    @champion = Champion.new
+    respond_to do |format|
+      format.html
+      format.json do
+        @category_children = Category.find(params[:tournament_id]).children
+      end
+    end
+  end
+
+  def create
+    @champion = Champion.create(champion_params)
+    return redirect_to champions_path if @champion.save
+    render "new"
+  end
+
   def show
+  end
+
+  private
+  def set_category
+    @category_parent_array = Category.where(ancestry: nil)
   end
 end
