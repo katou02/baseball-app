ba954457f1fcb09c08f45418e0b5aae491282f0a
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-13 09:31:38 +0900

    エラーメッセージ

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,4 +12,5 @@ class Category < ApplicationRecord
   has_many :win_school_forecasts,class_name: 'Forecast',  foreign_key: 'win_school_id'
   has_many :lose_school_forecasts,class_name: 'Forecast',  foreign_key: 'lose_school_id'
   has_many :tournament_forecasts,class_name: 'Forecast',  foreign_key: 'tournament_id'
+  has_many :tournament_champions,class_name: 'Champion',  foreign_key: 'tournament_id'
 end
