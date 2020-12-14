3781a71fd64169a51dc09b5cfcf8f23095aa77a2
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-14 23:07:24 +0900

    S３保存

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,18 +3,17 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? 
-  elsif Rails.env.production?
+  # if Rails.env.development? || Rails.env.test? 
+  # elsif Rails.env.production?
   config.storage = :fog
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID2'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY2'],
     region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'rails-baseball-app'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/rails-baseball-app'
   end
-end
