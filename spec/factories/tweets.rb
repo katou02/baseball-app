d8feaca9b87f2af2455487dd5437b54f882dd700
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-22 16:29:43 +0900

    id追加

---
+++ spec/factories/tweets.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :tweet do
+    title_info { "テスト" }
+    school_a_score { "1" }
+    school_b_score { "2" }
+    text {"テキスト"}
+    association :school_a,factory: :category
+    association :school_b,factory: :category_2
+    association :tournament,factory: :tournament
+    association :user,factory: :user
+  end
+end
