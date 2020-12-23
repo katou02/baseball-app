8b415bbd66ab8fbf25d256e41c930a2a4c29ceb7
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-23 15:46:16 +0900

    数値変更

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :tweet do
     title_info { "テスト" }
-    school_a_score { "1" }
-    school_b_score { "2" }
+    school_a_score { 1 }
+    school_b_score { 2 }
     text {"テキスト"}
     association :school_a,factory: :category
     association :school_b,factory: :category_2
