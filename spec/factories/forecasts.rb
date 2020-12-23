7c1876a68073a6b699a3e8a93b96fc3d10034282
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-23 17:39:10 +0900

    Forecastモデルテスト

---
+++ spec/factories/forecasts.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :forecast do
+    text { "テキスト" }
+    round { "1回戦" }
+    probability {"80%"}
+    association :win_school,factory: :category
+    association :lose_school,factory: :category_2
+    association :tournament,factory: :tournament
+    association :user,factory: :user
+  end
+end
