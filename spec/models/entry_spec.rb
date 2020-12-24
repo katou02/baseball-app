b29580afe87ac2befb098d03e74901a105ad39d4
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-24 14:52:32 +0900

    モデルテスト

---
+++ spec/models/entry_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Entry, type: :model do
+  describe 'アソシエーションテスト' do
+    
+    context 'Userテーブルとの関係' do
+      it '1:Nとなっている' do
+        t = Entry.reflect_on_association(:user)
+        expect(t.macro).to eq(:belongs_to)
+      end
+    end
+
+    context 'Roomテーブルとの関係' do
+      it '1:Nとなっている' do
+        t = Entry.reflect_on_association(:room)
+        expect(t.macro).to eq(:belongs_to)
+      end
+    end
+  end
+end
