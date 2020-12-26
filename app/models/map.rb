1bea09abbb7d8425a86c4955f7f33b2d374a8932
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-26 11:07:51 +0900

    Update map.rb

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,8 +1,10 @@
 class Map < ApplicationRecord
   belongs_to :user
-  has_many :categories
   belongs_to :school,class_name: 'Category', foreign_key: 'school_id'
+  belongs_to :tournament,class_name: 'Category', foreign_key: 'tournament_id'
+  has_many :categories
   validates :text,length: {maximum: 2000}
+  # 緯度経度
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 end
