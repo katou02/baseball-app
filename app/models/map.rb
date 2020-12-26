2dfdcd85840f54148a8e7f7cd827f4f6aa8a1839
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-26 10:33:08 +0900

    Update map.rb

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,4 +1,7 @@
 class Map < ApplicationRecord
+  belongs_to :user
+  has_many :categories
+  belongs_to :school,class_name: 'Category', foreign_key: 'school_id'
   validates :text,length: {maximum: 2000}
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
