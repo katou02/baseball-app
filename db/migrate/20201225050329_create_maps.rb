84234ee2b01b44884be1242cf6362f296ac3077f
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2020-12-26 10:19:17 +0900

    追加

--- db/migrate/20201225050329_create_maps.rb
+++ db/migrate/20201225050329_create_maps.rb
@@ -1,11 +1,13 @@
 class CreateMaps < ActiveRecord::Migration[5.2]
   def change
     create_table :maps do |t|
-      t.references :school, foreign_key: { to_table: :categories }, null: false
       t.text :text
       t.string :address
       t.float :latitude
       t.float :longitude
+      t.references :school, foreign_key: { to_table: :categories }, null: false
+      t.references :tournament, foreign_key: { to_table: :categories }, null: false
+      t.integer :user_id
       t.timestamps
     end
   end
