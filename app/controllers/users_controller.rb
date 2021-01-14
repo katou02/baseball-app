595a0bb612daa7f33ac628e1cd7e626a9dd6f5e1
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-01-15 00:00:04 +0900

    ページネーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!,only: [:show]
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(16).order("created_at DESC")
   end
   
   def following
