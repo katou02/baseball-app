f50b7673aed998661b10d822edf2022b9ea4e42e
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-02-13 09:52:15 +0900

    ファイル名変更

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
-import App from '../app.vue'
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import App from '../app.vue';
+
+Vue.use(Vuetify);
+const vuetify = new Vuetify();
 
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.body.appendChild(document.createElement('tweet'))
   const app = new Vue({
     el,
+    vuetify,
     render: h => h(App)
   })
 })
