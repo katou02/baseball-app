a6365544d69b372cd7138bfb147c43a979fa23f5
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-02-19 01:07:34 +0900

    Vuetify

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,16 +1,15 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 import App from '../app.vue';
-
 Vue.use(Vuetify);
-const vuetify = new Vuetify();
 
 document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('tweet'))
+  // const el = document.body.appendChild(document.createElement('tweet'))
   const app = new Vue({
-    el,
-    vuetify,
+    // el,
+    vuetify: new Vuetify(),
     render: h => h(App)
-  })
+  }).$mount();
+  document.body.appendChild(app.$el);
 })
