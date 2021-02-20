a194978c930eb3338a401de4e52ef0beda470b97
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-02-20 17:00:08 +0900

    API JSONで取得

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -6,7 +6,11 @@ class Api::V1::TweetsController < ApiController
   end
 
   def index
-    @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+    # @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+    page = params[:page] || 1
+    per = params[:per] || 5
+    @tweets = Tweet.page(page).per(per)
+    @total_pages = @tweets.total_pages
     render 'index', formats: 'json', handlers: 'jbuilder'
   end
 
