2cc0eae7eb1cf7d7053aa07650478b296ba3f1c4
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-02-25 20:46:05 +0900

    大会名

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,11 +1,12 @@
 class TournamentsController < ApplicationController
+  before_action :set_category, except: [:average,:watch_avg] 
+
   def show
     @tweets = Tweet.where(tournament_id: params[:id]).includes(:user).page(params[:page]).per(5).order("created_at DESC")
     @tweet = Tweet.find_by(tournament_id: params[:id])
   end
 
   def watch_ays
-    @analysis = Category.find(params[:id])
     @analyses = Analysis.where(tournament_id: params[:id]).includes(:user).page(params[:page]).per(5).order("created_at DESC")
     @analysis_tournament = Analysis.find_by(tournament_id: params[:id])
   end
@@ -17,7 +18,6 @@ class TournamentsController < ApplicationController
 
   def watch_fcs
     @forecasts = Forecast.where(tournament_id: params[:id]).includes(:user).page(params[:page]).per(5).order("created_at DESC")
-    @category = Category.find(params[:id])
   end
   
   def average(num)
@@ -31,5 +31,10 @@ class TournamentsController < ApplicationController
     @avgs = sum.group_by{|x| x[:name]}
     .map{|k,x| {name: k, attack_num: x.sum(0.0){|y| y[:attack_num]}/x.size,defense_num: x.sum(0.0){|y| y[:defense_num]}/ x.size,pitch_num: x.sum(0.0){|y| y[:pitch_num]}/ x.size,total_num: x.sum(0.0){|y| y[:total_num]}/ x.size,
                 expectations_num: x.sum(0.0){|y| y[:expectations_num]}/x.size}}.to_a
+  end
+
+  private
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
