a13122ebb77d6dd8f54709dcaaa3fb1ae9c99516
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-03-11 23:13:49 +0900

    コメント　非同期

--- app/controllers/comment_forecasts_controller.rb
+++ app/controllers/comment_forecasts_controller.rb
@@ -1,16 +1,21 @@
 class CommentForecastsController < ApplicationController
   def create
-    comment = CommentForecast.create(comment_params)
-    redirect_to "/forecasts/#{comment.forecast.id}"
-    @forecast = comment.forecast
-    @forecast.create_notification_comment_forecast!(current_user, comment.id)
+    # comment = CommentForecast.create(comment_params)
+    # redirect_to "/forecasts/#{comment.forecast.id}"
+    # @forecast = comment.forecast
+    # @forecast.create_notification_comment_forecast!(current_user, comment.id)
+    @forecast = Forecast.find(params[:forecast_id])
+    @num = 1
+    @comment = @forecast.comment_forecasts.build(comment_params)
+    @comment.save
+    render :comment
   end
 
   def destroy
-    comment = CommentForecast.find(params[:id])
-    if comment.user_id = current_user.id
-      comment.destroy
-      redirect_to "/forecasts/#{comment.forecast.id}"
+    @comment = CommentForecast.find(params[:id])
+    if @comment.user_id = current_user.id
+      @comment.destroy
+      render :comment
     end
   end
 
