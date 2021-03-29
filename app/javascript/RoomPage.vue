050e3886f695c68380a78f1348ce9ff9bab5c4d0
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-03-29 13:56:38 +0900

    検索機能

--- app/javascript/RoomPage.vue
+++ app/javascript/RoomPage.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="user-content pt-5">
     <h2>DM一覧</h2>
+    <div class="search-area mt-3">
+      <input type="text" v-model="keyword" placeholder="検索">
+    </div>
     <div class="user-list row">
-      <div v-for="e in rooms" :key="e.id">
+      <div v-for="e in getLists" :key="e.id">
         <div class="col-xs-12 col-md-6 col-lg-3 mt-3 card">
           <a :href= "'/rooms/' + e.room_id">{{e.user}}とのDM
             <div class="user-list-image border-bottom">
@@ -16,6 +19,21 @@
           </a>
         </div>
       </div>
+      <div class="text-center">
+        <paginate
+          :v-model="currentPage" 
+          :page-count="getPageCount"
+          :click-handler="clickCallback"
+          :page-range="3"
+          :margin-pages="2"
+          :prev-text="'＜'"
+          :next-text="'＞'"
+          :next-link-class="'page-link'"
+          :prev-link-class="'page-link'"
+          :container-class="'pagination'"
+          :page-link-class="'page-link'">
+        </paginate>
+      </div>
     </div>
   </div>
 </template>
@@ -24,7 +42,10 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      rooms: []
+      rooms: [],
+      keyword: '',
+      currentPage: 1,
+      parPage: 1
     }
   },
   mounted() {
@@ -37,6 +58,35 @@ export default {
         .then(response =>{
           this.rooms = response.data;
         })
+    },
+    clickCallback(pageNum) {
+      this.currentPage = Number(pageNum);
+    }
+  },
+  computed: {
+    getRooms: function() {
+      let rooms = [];
+      for(let i in this.rooms) {
+        let room = this.rooms[i];
+        if( room.user.indexOf(this.keyword) !== -1) {
+          rooms.push(room);
+        }
+      }
+      return rooms
+    },
+    getLists: function() {
+      let current = this.currentPage * this.parPage;
+      let start = current - this.parPage;
+      return this.getRooms.slice(start, current);
+    },
+    getPageCount: function() {
+      return Math.ceil(this.getRooms.length / this.parPage);
+    }
+  },
+  watch: {
+    keyword: function(){
+      this.currentPage = 1;
+      // this.$store.state.keyword = this.keyword
     }
   }
 }
