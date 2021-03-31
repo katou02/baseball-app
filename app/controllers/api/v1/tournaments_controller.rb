030c918e097deef3f39a094c12be6a875d5c6221
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-03-31 21:02:42 +0900

    平均 json

--- app/controllers/api/v1/tournaments_controller.rb
+++ app/controllers/api/v1/tournaments_controller.rb
@@ -21,4 +21,23 @@ class Api::V1::TournamentsController < ApiController
     @category = Category.find(params[:id])
     render 'watch_fcs', formats: 'json', handlers: 'jbuilder'
   end
+
+  def watch_avg
+    @analyses = Analysis.where(tournament_id: params[:id]).includes(:user).page(params[:page]).per(5).order("created_at DESC")
+    average(@analyses)
+    render 'watch_avg', formats: 'json', handlers: 'jbuilder'
+  end
+
+  def average(num)
+    sum = []
+    num.each do |analysis|
+      sum << {
+        name: analysis.school.name,attack_num: analysis.attack,defense_num: analysis.defensive,
+        pitch_num: analysis.pitcher,total_num: analysis.comprehensive,expectations_num:analysis.expectations
+      }
+    end
+    @avgs = sum.group_by{|x| x[:name]}
+    .map{|k,x| {name: k, attack_num: x.sum(0.0){|y| y[:attack_num]}/x.size,defense_num: x.sum(0.0){|y| y[:defense_num]}/ x.size,pitch_num: x.sum(0.0){|y| y[:pitch_num]}/ x.size,total_num: x.sum(0.0){|y| y[:total_num]}/ x.size,
+                expectations_num: x.sum(0.0){|y| y[:expectations_num]}/x.size}}.to_a
+  end
 end
---
+++ app/views/api/v1/tournaments/watch_avg.jbuilder
@@ -0,0 +1,1 @@
+json.avgs @avgs
