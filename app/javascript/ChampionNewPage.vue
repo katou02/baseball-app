117b92dd93ed5687f08c7135329bd89e1e5c9cdd
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-03 14:53:00 +0900

    学校一覧取得

--- app/javascript/ChampionNewPage.vue
+++ app/javascript/ChampionNewPage.vue
@@ -2,3 +2,26 @@
   <div class="contents row">
   </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      schools: []
+    }
+  },
+  mounted() {
+    this.fetchSchools()
+  },
+  methods: {
+    fetchSchools() {
+      axios
+        .get(`/api/v1/champions/${this.$route.query.tournament_id}.json`)
+        .then(response => {
+          alert('tt')
+          this.schools = response.data.select_schools
+        })
+    }
+  }
+}
+</script>
