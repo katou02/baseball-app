b64876ac3caa7afe70b38df32f9c213130cc518d
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-05 10:29:45 +0900

    カテゴリ json

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -10,6 +10,24 @@ class Api::V1::ForecastsController < ApiController
     render 'index', formats: 'json', handlers: 'jbuilder'
   end
 
+  def new
+    @roots = Category.roots
+    root_id = params[:root_id]
+    child_id = params[:child_id]
+    @children = root_id ? @roots.find(root_id).children : []
+    @grand_children = child_id ? @children.find(child_id).children : []
+    render 'new', formats: 'json', handlers: 'jbuilder'
+  end
+
+  def create
+    forecast = Forecast.create(forecast_params)
+    if forecast.save
+      render json: forecast, status: :created
+    else
+      render json: { errors: forecast.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   def show
     @current_user = current_user
     @user = User.find_by(id: @forecast.user.id)
@@ -29,5 +47,11 @@ class Api::V1::ForecastsController < ApiController
 
   def search_forecast
     @forecast = Forecast.find(params[:id])
+  end
+
+  private
+
+  def forecast_params
+    params.require(:forecast).permit(:text,:round,:win_school_id,:lose_school_id,:tournament_id,:probability).merge(user_id: current_user.id)
   end
 end
