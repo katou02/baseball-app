7d62f5e847d9b8f38978cc60d62d79ee3ad16415
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-12 23:24:59 +0900

    ページネーション 検索

--- app/javascript/packs/store.js
+++ app/javascript/packs/store.js
@@ -10,8 +10,10 @@ const store = new Vuex.Store({
     currentPage_t: 1,
     currentPage_wa: 1,
     currentPage_wf: 1,
+    currentPage_follow: 1,
     tweets: [],
     keyword: '',
+    keyword_follow: '',
     keyword_fcs: '',
     keyword_ays: '',
     keyword_tour: '',
@@ -23,10 +25,12 @@ const store = new Vuex.Store({
     increment(state) {
       state.currentPage_t = 1
       state.currentPage_wa = 1
+      state.currentPage_follow = 1
       state.currentPage_wf = 1
       state.keyword_tour = ''
       state.keyword_wa = ''
       state.keyword_wf = ''
+      state.keyword_follow = ''
     }
   }
 })
