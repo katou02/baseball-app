19ac771dd90314dee9df9dea1836321f49683bc2
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-19 00:01:55 +0900

    通知取得条件の変更

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::NotificationsController < ApiController
   def index
-      @notifications = current_user.passive_notifications.page(params[:page]).per(10).order("created_at DESC")
+      # @notifications = current_user.passive_notifications.page(params[:page]).per(10).order("created_at DESC")
+      @notifications = Notification.where(visited_id: current_user.id)
       @check = @notifications.where(checked: false)
       @notifications.where(checked: false).each do |notification|
           notification.update_attributes(checked: true)
