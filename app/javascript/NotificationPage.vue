f44aa898eecf64b8f28a04a6306e93a6028d9a17
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-19 00:02:28 +0900

    APIで通知を取得

--- app/javascript/NotificationPage.vue
+++ app/javascript/NotificationPage.vue
@@ -1,5 +1,50 @@
 <template>
-  <div class="aaa">
-    
+  <div class="user-view">
+    <div class="notification-info">
+      <div v-for="e in notifications" :key="e.id">
+        <router-link :to="{name: 'user-show',params: {id: e.visiter_id}}">{{e.visiter}}</router-link>が
+        <span v-if="e.action==='follow'">
+          あなたをフォローしました
+        </span>
+        <span v-else-if="e.action==='like'">
+          あなたの<router-link :to="{name: 'tweetshow',params: {id: e.tweet}}">投稿</router-link>にいいねをしました
+        </span>
+        <span v-else-if="e.action==='comment'">
+          あなたの<router-link :to="{name: 'tweetshow',params: {id: e.tweet}}">投稿</router-link>にコメントをしました
+        </span>
+        <span v-else-if="e.action==='comment_analysis'">
+          あなたの<router-link :to="{name: 'analysis-show',params: {id: e.analysis}}">投稿</router-link>にコメントをしました
+        </span>
+        <span v-else-if="e.action==='comment_forecast'">
+          あなたの<router-link :to="{name: 'forecast-show',params: {id: e.forecast}}">投稿</router-link>にコメントをしました
+        </span>
+        <span v-else-if="e.action==='dm'">
+          あなたに<router-link :to="{name: 'room-show',params: {id: e.room}}">メッセージ</router-link>を送りました
+        </span>
+        {{e.time+"前"}}
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      notifications: []
+    }
+  },
+  mounted() {
+    this.fetchNotifications()
+  },
+  methods: {
+    fetchNotifications() {
+      axios
+        .get('api/v1/notifications')
+        .then(response =>{
+          this.notifications = response.data.notification;
+        })
+    }
+  }
+}
+</script>
--- app/views/api/v1/notifications/index.json.jbuilder
+++ app/views/api/v1/notifications/index.json.jbuilder
@@ -1,16 +1,19 @@
-# json.array! @notifications do |notification|
-#   json.tweet notification.tweet_id
-#   json.analysis notification.analysis_id
-#   json.forecast notification.forecast_id
-#   json.room notification.room_id
-#   json.message notification.message_id
-#   json.comment notification.comment_id
-#   json.comment_analysis notification.comment_analysis_id
-#   json.comment_forecast notification.comment_forecast_id
-#   json.action notification.action
-# end
 json.notification do
-  json.array! @notifications
+  json.array! @notifications do |notification|
+    json.id notification.id
+    json.tweet notification.tweet_id
+    json.analysis notification.analysis_id
+    json.forecast notification.forecast_id
+    json.room notification.room_id
+    json.message notification.message_id
+    json.comment notification.comment_id
+    json.comment_analysis notification.comment_analysis_id
+    json.comment_forecast notification.comment_forecast_id
+    json.action notification.action
+    json.visiter notification.visiter.nickname
+    json.visiter_id notification.visiter.id
+    json.time time_ago_in_words(notification.created_at)
+  end
 end
 
 json.check do
