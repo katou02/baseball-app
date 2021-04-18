d5b925bc107b0598eceb29b1dc2d614c395f818c
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-18 16:33:29 +0900

    トップページ仮

---
+++ app/javascript/TopIndexPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="top">
+    <router-link :to="{name: 'tweet'}">試合記事</router-link>
+    <router-link :to="{name: 'analysis'}">戦力分析</router-link>
+    <router-link :to="{name: 'forecast'}">試合予想</router-link>
+  </div>
+</template>
