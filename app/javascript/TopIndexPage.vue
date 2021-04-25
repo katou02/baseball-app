cdfd7fe6b2ee53ddd143fefb827624865a9fe7d2
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-25 16:33:44 +0900

    ヘッダー追加

--- app/javascript/TopIndexPage.vue
+++ app/javascript/TopIndexPage.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="top">
+    <Header></Header>
     <router-link :to="{name: 'tweet'}">試合記事</router-link>
     <router-link :to="{name: 'analysis'}">戦力分析</router-link>
     <router-link :to="{name: 'forecast'}">試合予想</router-link>
   </div>
 </template>
+<script>
+import Header from './components/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
