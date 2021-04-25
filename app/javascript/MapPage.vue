9ce6b652ae3a536fcf8430eea067b90290420709
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-25 16:02:29 +0900

    スペース変更

--- app/javascript/MapPage.vue
+++ app/javascript/MapPage.vue
@@ -25,39 +25,36 @@
         </div>
       </div>
     </div>
-    <div class="search-area">
+    <div class="search-area mt-5">
       <input type="text" v-model="keyword" placeholder="検索">
     </div>
     <div class="d-flex">
       <Side></Side>
-      <!-- <div class="map-list row mt-5 mx-auto"> -->
-        <v-row>
-          <v-col cols="12"  sm="6" md="6" lg="4" v-for="e in getLists" :key="e.id" class="mt-3">
-            <div class="map-deta">
-              <router-link :to="{name: 'mapshow',params: {id: e.id}}">
-                <div class="d-flex h-100">
-                  <!-- <div class="map-user_image"> -->
-                  <div v-if="e.image.url"><img :src="e.image.url" class="article-icon"></div>
-                  <div v-else><img src="/images/ball.jpg" class="article-icon"></div>
-                  <div class="article-heading mx-auto bg-white">
-                    <div class="name">
-                      投稿者
-                      {{e.nickname}}<br>
-                      {{e.time}}
-                    </div>
-                    <div class="sub-title">
-                      {{e.school}}のふるさと
-                    </div>
-                    <div class="text-center">
-                      <img src="/images/hurusato.jpeg" width="50px" height="auto" >
-                    </div>
+      <v-row>
+        <v-col cols="12"  sm="6" md="6" lg="4" v-for="e in getLists" :key="e.id">
+          <div class="map-deta mt-5">
+            <router-link :to="{name: 'mapshow',params: {id: e.id}}">
+              <div class="d-flex h-100">
+                <div v-if="e.image.url"><img :src="e.image.url" class="article-icon"></div>
+                <div v-else><img src="/images/ball.jpg" class="article-icon"></div>
+                <div class="article-heading mx-auto bg-white">
+                  <div class="name">
+                    投稿者
+                    {{e.nickname}}<br>
+                    {{e.time}}
+                  </div>
+                  <div class="sub-title">
+                    {{e.school}}のふるさと
+                  </div>
+                  <div class="text-center">
+                    <img src="/images/hurusato.jpeg" width="50px" height="auto" >
                   </div>
                 </div>
-              </router-link>
-            </div>
-          </v-col>
-        </v-row>
-      <!-- </div> -->
+              </div>
+            </router-link>
+          </div>
+        </v-col>
+      </v-row>
     </div>
     <div class="text-center">
       <paginate
