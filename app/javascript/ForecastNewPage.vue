b2127f67886a254ff13d318ea1f82e22a1fec095
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-28 14:10:45 +0900

    デザイン変更

--- app/javascript/ForecastNewPage.vue
+++ app/javascript/ForecastNewPage.vue
@@ -1,49 +1,81 @@
 <template>
-  <div class="contents row mt-2">
-    <router-link :to="{name: 'forecast'}" class="return-btn">戻る</router-link>
-    <h2>試合記事の投稿</h2>
+  <div class="contents row mx-auto mt-2">
+    <!-- <router-link :to="{name: 'forecast'}" class="return-btn">戻る</router-link> -->
+    <h2>試合予想の投稿</h2>
     <form @submit.prevent="createForecast">
       <div class="form p-4">
-        <div class="select-tournament">
+        <div class="select-tournament w-50 mx-auto">
           <label>大会名</label><br>
-          <select @change="findChildren" v-model="tournament">
+          <select @change="findChildren" v-model="tournament" class="border">
             <option disabled value="">大会を選択</option>
             <option v-for="root in roots" :value="root.id" :key="root.id">{{ root.name }}</option>
           </select>
         </div>
-        <p v-if="!!errors['tournament']" class="error" style="color: red;">{{ errors['tournament'][0]}}</p>
-        <div class="win-school mt-3">
-          <ul>
-            <label>勝利予想</label><br>
-            <select @change="findGrandChildren" v-model="win_school">
-              <option v-for="child in children" :value="child.id" :key="child.id">{{ child.name }}</option>
-            </select>
-          </ul>
+        <p v-if="!!errors['tournament']" style="color: red;">{{ errors['tournament'][0]}}</p>
+        <div class="d-flex">
+          <div class="win-school w-25 mx-auto mt-3">
+            <ul>
+              <label>勝利予想</label><br>
+              <!-- <select @change="findGrandChildren" v-model="win_school">
+                <option v-for="child in children" :value="child.id" :key="child.id">{{ child.name }}</option>
+              </select> -->
+              <v-select
+                v-model="win_school"
+                @change="findGrandChildren" 
+                item-text="name"
+                item-value="id"
+                :items="children"
+                outlined>
+              </v-select>
+            </ul>
+          </div>
+          <div class="lose-school w-25 mx-auto mt-3">
+            <ul>
+              <label>敗退予想</label><br>
+              <!-- <select @change="findGrandChildren" v-model="lose_school">
+                <option v-for="child in children" :value="child.id" :key="child.id">{{ child.name }}</option>
+              </select> -->
+              <v-select
+                v-model="lose_school"
+                @change="findGrandChildren" 
+                item-text="name"
+                item-value="id"
+                :items="children"
+                outlined>
+              </v-select>
+            </ul>
+          </div>
         </div>
-        <div class="lose-school mt-3">
-          <ul>
-            <label>敗退予想</label><br>
-            <select @change="findGrandChildren" v-model="lose_school">
-              <option v-for="child in children" :value="child.id" :key="child.id">{{ child.name }}</option>
-            </select>
-          </ul>
-        </div>
-        <div class="round mt-3">
+        <div class="round w-25 mx-auto mt-3">
           <label>ラウンド</label><br>
-          <select v-model="round">
+          <!-- <select v-model="round">
             <option v-for="round in round_list" :value="round.round" :key="round.id">{{ round.round }}</option>
-          </select>
+          </select> -->
+            <v-select
+              v-model="round"
+              item-text="round"
+              item-value="round"
+              :items="round_list"
+              outlined>
+            </v-select>
         </div>
-        <div class="win-rate mt-3">
+        <div class="win-rate w-25 mx-auto mt-3">
           <label>勝利予想チームの勝利確率</label><br>
-          <select v-model="probability">
+          <!-- <select v-model="probability">
             <option v-for="probability in probability_list" :value="probability.probability" :key="probability.id">{{ probability.label }}</option>
-          </select>
+          </select> -->
+          <v-select
+            v-model="probability"
+            item-text="label"
+            item-value="probability"
+            :items="probability_list"
+            outlined>
+          </v-select>
         </div>
       </div>
-      <textarea v-model="text" type="text" rows="2" cols="30" placeholder="本文"></textarea>
-      <p v-if="!!errors['text']" class="error" style="color: red;">{{ errors['text'][0]}}</p>
-      <button type="submit" class="game_record" >投稿する</button>
+      <v-textarea v-model="text" type="text" label="本文" outlined></v-textarea>
+      <p v-if="!!errors['text']" style="color: red;">{{ errors['text'][0]}}</p>
+      <v-btn type="submit" color="primary" class="text-white mt-5">投稿する</v-btn>
     </form>
   </div>
 </template>
