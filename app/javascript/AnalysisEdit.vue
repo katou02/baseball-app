a6e9865a279b788610cf7a996e5593ea68bd7ca7
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-29 22:45:42 +0900

    デザイン変更

--- app/javascript/AnalysisEdit.vue
+++ app/javascript/AnalysisEdit.vue
@@ -1,54 +1,70 @@
 <template>
-  <div class="contents row">
+  <div class="contents row mx-auto mt-2">
+    <h2 class="text-primary font-weight-bold">戦力分析の編集</h2>
+    <v-divider></v-divider>
     <form @submit.prevent="editAnalysis">
       {{tournament}}
       <div class="select-from">
-        <div class="select-school mt-3">
+        <div class="select-school w-25 mx-auto mt-3">
           <ul>
-            <label>高校</label><br>
-            <select v-model="school">
-              <option v-for="child in children" :value="child.id" :key="child.id">{{ child.name }}</option>
-            </select>
+            <label>学校</label><br>
+            <v-select
+              v-model="school"
+              @change="findGrandChildren" 
+              item-text="name"
+              item-value="id"
+              :items="children"
+              outlined>
+            </v-select>
           </ul>
+          <p v-if="!!errors['school']" style="color: red;">{{ errors['school'][0]}}</p>
         </div>
         <br><p>5段階評価</p>
         <div class="select">
-          <div class="attack">
+          <div class="attack w-25">
             <label>攻撃力</label><br>
-            <select v-model="attack">
-              <option v-for="attack in 5" :value="attack" :key="attack.id">{{ attack }}</option>
-            </select>
+            <v-select
+              v-model="attack"
+              :items="score"
+              solo>
+            </v-select>
           </div>
-          <div class="defensive">
+          <div class="defensive w-25">
             <label>守備力</label><br>
-            <select v-model="defensive">
-              <option v-for="defensive in 5" :value="defensive" :key="defensive.id">{{ defensive }}</option>
-            </select>
+            <v-select
+              v-model="defensive"
+              :items="score"
+              solo>
+            </v-select>
           </div>
-          <div class="pitcher">
+          <div class="pitcher w-25">
             <label>投手力</label><br>
-            <select v-model="pitcher">
-              <option v-for="pitcher in 5" :value="pitcher" :key="pitcher.id">{{ pitcher }}</option>
-            </select>
+            <v-select
+              v-model="pitcher"
+              :items="score"
+              solo>
+            </v-select>
           </div>
-          <div class="comprehensive">
+           <div class="comprehensive w-25">
             <label>総合力</label><br>
-            <select v-model="comprehensive">
-              <option v-for="comprehensive in 5" :value="comprehensive" :key="comprehensive.id">{{ comprehensive }}</option>
-            </select>
+            <v-select
+              v-model="comprehensive"
+              :items="score"
+              solo>
+            </v-select>
           </div>
-          <div class="expectations">
+          <div class="expectations w-25">
             <label>期待度</label><br>
-            <select v-model="expectation">
-              <option v-for="expectation in 5" :value="expectation" :key="expectation.id">{{ expectation }}</option>
-            </select>
+            <v-select
+              v-model="expectation"
+              :items="score"
+              solo>
+            </v-select>
           </div>
         </div>
-        <input v-model="title" type="text" rows="2" cols="30" placeholder="タイトル 30字以内" class="game_title">
-        <p v-if="!!errors['title']" class="error" style="color: red;">{{ errors['title'][0]}}</p>
-        <textarea v-model="text" type="text" rows="2" cols="30" placeholder="本文"></textarea>
-        <p v-if="!!errors['text']" class="error" style="color: red;">{{ errors['text'][0]}}</p>
-        <button type="submit" class="game_record" >投稿する</button>
+        <v-text-field v-model="title" type="text" label="タイトル 30字以内" class="game_title"></v-text-field>
+        <v-textarea v-model="text" type="text" label="本文"></v-textarea>
+        <v-btn type="submit" color="primary" class="text-white mt-5">編集する</v-btn>
       </div>
     </form>
   </div>
@@ -56,6 +72,8 @@
 
 <script>
 import axios from 'axios';
+const maxscore = 6;
+const score = [...Array(maxscore).keys()]
 export default {
   data: function() {
     return {
@@ -73,6 +91,7 @@ export default {
       expectation: '',
       title: '',
       text: '',
+      score: score,
       errors: ''
     }
   },
