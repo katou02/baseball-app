3280097fc5abf2cfaf1cebee9004b090d28a4b90
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-29 20:58:17 +0900

    デザイン変更

--- app/javascript/TweetEditPage.vue
+++ app/javascript/TweetEditPage.vue
@@ -1,50 +1,82 @@
 <template>
-  <div class="contents row mt-2">
-    <h2>試合記事の編集</h2>
-    <form @submit.prevent="editTweet">
-      <div class="select-from">
-        {{tournament}}
-        <div class="containe_r p-4">
-          <div class="select-school mt-3">
-            <ul>
-              <label>高校A</label><br>
-              <select v-model="school_a">
-                <option v-for="child in children" :value="child.id" :key="child.id">{{ child.name }}</option>
-             </select>
-            </ul>
+  <div class="contents row mx-auto">
+    <v-container>
+      <h2 class="text-primary font-weight-bold">試合記事の編集</h2>
+      <v-divider></v-divider>
+      <form @submit.prevent="editTweet">
+        <div class="select-from">
+          {{tournament}}
+          <div class="containe_r p-4">
+            <div class="d-flex">
+              <div class="select-school w-25 mx-auto mt-3">
+                <ul>
+                  <label>高校A</label><br>
+                  <!-- <select @change="findGrandChildren" v-model="school_a">
+                    <option v-for="child in children" :value="child.id" :key="child.id">{{ child.name }}</option>
+                  </select> -->
+                  <v-select
+                    v-model="school_a"
+                    @change="findGrandChildren" 
+                    item-text="name"
+                    item-value="id"
+                    :items="children"
+                    label="高校を選択"
+                    outlined>
+                  </v-select>
+                </ul>
+              </div>
+              <div class="select-school w-25 mx-auto mt-3">
+                <ul>
+                  <label>高校B</label><br>
+                  <!-- <select @change="findGrandChildren" v-model="school_b">
+                    <option v-for="child in children" :value="child.id" :key="child.id">{{ child.name }}</option>
+                  </select> -->
+                  <v-select
+                    v-model="school_b"
+                    @change="findGrandChildren" 
+                    item-text="name"
+                    item-value="id"
+                    :items="children"
+                    label="高校を選択"
+                    outlined>
+                  </v-select>
+                </ul>
+              </div>
+            </div>
+            <div class="d-flex">
+              <div class="school-a_score w-25 mx-auto mt-3">
+                <label>高校A 得点</label><br>
+                <v-select
+                  v-model="school_a_score"
+                  :items="score"
+                  solo>
+                </v-select>
+                <p v-if="!!errors['school_a_score']" style="color: red;">{{ errors['school_a_score'][0]}}</p>
+              </div>
+              <div class="school-b_score w-25 mx-auto mt-3">
+                <label>高校B 得点</label><br>
+                <v-select
+                  v-model="school_b_score"
+                  :items="score"
+                  solo>
+                </v-select>
+              </div>
+            </div>
           </div>
-          <div class="select-school mt-3">
-            <ul>
-              <label>高校B</label><br>
-              <select v-model="school_b">
-                <option v-for="child in children" :value="child.id" :key="child.id">{{ child.name }}</option>
-              </select>
-            </ul>
-          </div>
-          <div class="school-a_score mt-3">
-            <label>高校A 得点</label><br>
-            <select v-model="school_a_score">
-              <option v-for="school_a_score in 50" :value="school_a_score" :key="school_a_score.id">{{ school_a_score }}</option>
-            </select>
-          </div>
-          <div class="school-b_score mt-3">
-            <label>高校B 得点</label><br>
-            <select v-model="school_b_score">
-              <option v-for="school_b_score in 50" :value="school_b_score" :key="school_b_score.id">{{ school_b_score }}</option>
-            </select>
-          </div>
+          <v-text-field v-model="title" type="text" label="タイトル 30字以内" class="game_title"></v-text-field>
+          <p v-if="!!errors['title_info']" class="error" style="color: red;">{{ errors['title_info'][0]}}</p>
+          <v-textarea v-model="text" type="text" label="本文" outlined></v-textarea>
+          <p v-if="!!errors['text']" class="error" style="color: red;">{{ errors['text'][0]}}</p>
+          <v-btn type="submit" color="primary" class="text-white mt-5">編集する</v-btn>
         </div>
-        <input v-model="title" type="text" rows="2" cols="30" placeholder="タイトル 30字以内" class="game_title">
-        <p v-if="!!errors['title_info']" class="error" style="color: red;">{{ errors['title_info'][0]}}</p>
-        <textarea v-model="text" type="text" rows="2" cols="30" placeholder="本文"></textarea>
-        <p v-if="!!errors['text']" class="error" style="color: red;">{{ errors['text'][0]}}</p>
-        <button type="submit" class="game_record" >編集する</button>
-      </div>
-    </form>
+      </form>
+    </v-container>
   </div>
 </template>
 <script>
 import axios from 'axios';
+const maxscore = 51;
+const score = [...Array(maxscore).keys()]
 export default {
   data: function() {
     return {
@@ -55,6 +87,7 @@ export default {
       child_id: '',
       school_a_score: '',
       school_b_score: '',
+      score: score,
       school_a: '',
       school_b: '',
       title: '',
