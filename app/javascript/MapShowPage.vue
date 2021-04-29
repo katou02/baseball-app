70d1c124e940cd64614702229c51b53f9b94253c
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-04-29 15:29:33 +0900

    デザイン変更

--- app/javascript/MapShowPage.vue
+++ app/javascript/MapShowPage.vue
@@ -2,38 +2,40 @@
   <div class="map">
     <div class="map-info">
       <div class="map-school">
-        {{map.school}}
+        {{map.school}}のふるさと
+      </div>
+      <div class="map-adress">
+        <p class="text-center pt-1">{{map.address}}</p>
+        <div id="map">
+          <GmapMap
+          :center="{lat: map.latitude, lng:map.longitude}"
+          :zoom="15"
+          map-type-id="terrain"
+          style="width: 100%; height: 300px"
+          >
+          <GmapMarker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="center=m.position"
+          />
+          </GmapMap>
+        </div>
       </div>
       <div class="map-text">
-        <p class="text-center pt-1">{{map.address}}</p>
-        <div class="mt-4 mb-4" style="white-space:pre-wrap;">
+        <div class="mt-4 mb-4 w-100" style="white-space:pre-wrap;">
           {{map.text}}
         </div>
-      </div>
-      <div class="map-image">
-        <div v-if="image">
-          <img :src= image class="map-image_content">
+        <div class="map-image">
+          <div class="h-100" v-if="image">
+            <img :src= image class="map-image_content">
+          </div>
+          <div class="h-100" v-else>
+            <img src="../assets/images/map-no-image.png" class="map-image_content">
+          </div>
         </div>
-        <div v-else>
-          <img src="../assets/images/map-no-image.png" class="map-image_content">
-        </div>
-      </div>
-      <div id="map">
-        <GmapMap
-        :center="{lat: map.latitude, lng:map.longitude}"
-        :zoom="15"
-        map-type-id="terrain"
-        style="width: 100%; height: 350px"
-        >
-        <GmapMarker
-        :key="index"
-        v-for="(m, index) in markers"
-        :position="m.position"
-        :clickable="true"
-        :draggable="true"
-        @click="center=m.position"
-        />
-        </GmapMap>
       </div>
     </div>
   </div>
