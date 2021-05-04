f3e6aae8b5a0b5cc3f6d2dd132ccd4384c0dbeef
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-05-04 09:26:17 +0900

    確認ダイアログ

--- app/javascript/ForecastShowPage.vue
+++ app/javascript/ForecastShowPage.vue
@@ -3,7 +3,7 @@
     <div class="data-info">
       <div class="d-flex">
         <div v-if="forecast.user_id==forecast.current_user">
-          <button class="delete-btn" @click="deleteForecast(forecast.id)">記事を削除する</button>
+          <button class="delete-btn" @click="onAlert">記事を削除する</button>
           <router-link :to="{name: 'forecast-edit',params: {id: forecast.id}}" class="edit-article text-white p-2">記事を編集する</router-link>
         </div>
         <router-link :to="{name: 'forecast'}" class="return-btn text-white">記事一覧へ戻る</router-link>
@@ -172,6 +172,15 @@ export default {
           }
         }
       });
+    },
+    onAlert:function(){
+      this.$dialog
+      var rt =confirm(
+      '削除してもよろしいですか？'
+      )
+      if(rt==true) {
+        this.deleteForecast(this.$route.params.id)
+      }
     }
   }
 }
