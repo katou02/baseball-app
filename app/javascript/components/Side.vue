bb235d5cbe09e834271ed1ef66a103fbe0b1b0ab
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-05-05 01:04:13 +0900

    サイドバー変更

--- app/javascript/components/Side.vue
+++ app/javascript/components/Side.vue
@@ -25,31 +25,31 @@
           <v-list-item>
             <v-list-item-title>
               <router-link :to="{name: 'tournament',params: {id: 1}}">
-                <v-icon>mdi-pencil</v-icon>第99回全国高校野球選手権
+                <v-icon>mdi-pencil</v-icon>第91回選抜高校野球大会
               </router-link>
             </v-list-item-title>
           </v-list-item>
 
           <v-list-item>
             <v-list-item-title>
-              <router-link :to="{name: 'tournament',params: {id: 51}}">
-                <v-icon>mdi-pencil</v-icon>第100回全国高校野球選手権
-              </router-link>
-            </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>
-              <router-link :to="{name: 'tournament',params: {id: 108}}">
-                <v-icon>mdi-pencil</v-icon>2020年甲子園高校野球交流試合
-              </router-link>
-            </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>
-              <router-link :to="{name: 'tournament',params: {id: 141}}">
+              <router-link :to="{name: 'tournament',params: {id: 117}}">
                 <v-icon>mdi-pencil</v-icon>第93回選抜高校野球大会
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>
+              <router-link :to="{name: 'tournament',params: {id: 34}}">
+                <v-icon>mdi-pencil</v-icon>第101回全国高校野球選手権大会
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>
+              <router-link :to="{name: 'tournament',params: {id: 84}}">
+                <v-icon>mdi-pencil</v-icon>2020年甲子園高校野球交流試合
               </router-link>
             </v-list-item-title>
           </v-list-item>
@@ -71,31 +71,31 @@
           <v-list-item>
             <v-list-item-title>
               <router-link :to="{name: 'watch_ays',params: {id: 1}}">
-                <v-icon>mdi-magnify</v-icon>第99回全国高校野球選手権
+                <v-icon>mdi-magnify</v-icon>第91回選抜高校野球大会
               </router-link>
             </v-list-item-title>
           </v-list-item>
 
           <v-list-item>
             <v-list-item-title>
-              <router-link :to="{name: 'watch_ays',params: {id: 51}}">
-                <v-icon>mdi-magnify</v-icon>第100回全国高校野球選手権
-              </router-link>
-            </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>
-              <router-link :to="{name: 'watch_ays',params: {id: 108}}">
-                <v-icon>mdi-magnify</v-icon>2020年甲子園高校野球交流試合
-              </router-link>
-            </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>
-              <router-link :to="{name: 'watch_ays',params: {id: 141}}">
+              <router-link :to="{name: 'watch_ays',params: {id: 117}}">
                 <v-icon>mdi-magnify</v-icon>第93回選抜高校野球大会
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>
+              <router-link :to="{name: 'watch_ays',params: {id: 34}}">
+                <v-icon>mdi-magnify</v-icon>第101回全国高校野球選手権大会
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>
+              <router-link :to="{name: 'watch_ays',params: {id: 84}}">
+                <v-icon>mdi-magnify</v-icon>2020年甲子園高校野球交流試合
               </router-link>
             </v-list-item-title>
           </v-list-item>
@@ -117,31 +117,31 @@
           <v-list-item>
             <v-list-item-title>
               <router-link :to="{name: 'watch_fcs',params: {id: 1}}">
-                <v-icon>mdi-scale-balance</v-icon>第99回全国高校野球選手権
+                <v-icon>mdi-scale-balance</v-icon>第91回選抜高校野球大会
               </router-link>
             </v-list-item-title>
           </v-list-item>
 
           <v-list-item>
             <v-list-item-title>
-              <router-link :to="{name: 'watch_fcs',params: {id: 51}}">
-                <v-icon>mdi-scale-balance</v-icon>第100回全国高校野球選手権
-              </router-link>
-            </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>
-              <router-link :to="{name: 'watch_fcs',params: {id: 108}}">
-                <v-icon>mdi-scale-balance</v-icon>2020年甲子園高校野球交流試合
-              </router-link>
-            </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>
-              <router-link :to="{name: 'watch_fcs',params: {id: 141}}">
+              <router-link :to="{name: 'watch_fcs',params: {id: 117}}">
                 <v-icon>mdi-scale-balance</v-icon>第93回選抜高校野球大会
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>
+              <router-link :to="{name: 'watch_fcs',params: {id: 34}}">
+                <v-icon>mdi-scale-balance</v-icon>第101回全国高校野球選手権大会
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>
+              <router-link :to="{name: 'watch_fcs',params: {id: 84}}">
+                <v-icon>mdi-scale-balance</v-icon>2020年甲子園高校野球交流試合
               </router-link>
             </v-list-item-title>
           </v-list-item>
@@ -155,31 +155,31 @@
           <v-list-item>
             <v-list-item-title>
               <router-link :to="{name: 'champion',params: {id: 1}}">
-                <v-icon>mdi-crown</v-icon>第99回全国高校野球選手権
+                <v-icon>mdi-crown</v-icon>第91回選抜高校野球大会
               </router-link>
             </v-list-item-title>
           </v-list-item>
 
           <v-list-item>
             <v-list-item-title>
-              <router-link :to="{name: 'champion',params: {id: 51}}">
-                <v-icon>mdi-crown</v-icon>第100回全国高校野球選手権
-              </router-link>
-            </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>
-              <router-link :to="{name: 'champion',params: {id: 108}}">
-                <v-icon>mdi-crown</v-icon>2020年甲子園高校野球交流試合
-              </router-link>
-            </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>
-              <router-link :to="{name: 'champion',params: {id: 141}}">
+              <router-link :to="{name: 'champion',params: {id: 117}}">
                 <v-icon>mdi-crown</v-icon>第93回選抜高校野球大会
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>
+              <router-link :to="{name: 'champion',params: {id: 34}}">
+                <v-icon>mdi-crown</v-icon>第101回全国高校野球選手権大会
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>
+              <router-link :to="{name: 'champion',params: {id: 84}}">
+                <v-icon>mdi-crown</v-icon>2020年甲子園高校野球交流試合
               </router-link>
             </v-list-item-title>
           </v-list-item>
@@ -193,31 +193,31 @@
           <v-list-item>
             <v-list-item-title>
               <router-link :to="{name: 'map',query: {tournament_id: 1}}">
-                <v-icon>mdi-city</v-icon>第99回全国高校野球選手権
+                <v-icon>mdi-city</v-icon>第91回選抜高校野球大会
               </router-link>
             </v-list-item-title>
           </v-list-item>
 
           <v-list-item>
             <v-list-item-title>
-              <router-link :to="{name: 'map',query: {tournament_id: 51}}">
-                <v-icon>mdi-city</v-icon>第100回全国高校野球選手権
-              </router-link>
-            </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>
-              <router-link :to="{name: 'map',query: {tournament_id: 108}}">
-                <v-icon>mdi-city</v-icon>2020年甲子園高校野球交流試合
-              </router-link>
-            </v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>
-              <router-link :to="{name: 'map',query: {tournament_id: 141}}">
+              <router-link :to="{name: 'map',query: {tournament_id: 117}}">
                 <v-icon>mdi-city</v-icon>第93回選抜高校野球大会
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>
+              <router-link :to="{name: 'map',query: {tournament_id: 34}}">
+                <v-icon>mdi-city</v-icon>第101回全国高校野球選手権大会
+              </router-link>
+            </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>
+              <router-link :to="{name: 'map',query: {tournament_id: 84}}">
+                <v-icon>mdi-city</v-icon>2020年甲子園高校野球交流試合
               </router-link>
             </v-list-item-title>
           </v-list-item>
