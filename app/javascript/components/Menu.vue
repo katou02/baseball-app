03144ccb3ee19a3b93870263aaf11f0e77580fd1
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-05-07 15:12:30 +0900

    ヘッダー作成

---
+++ app/javascript/components/Menu.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="menu-content">
+    <v-app-bar>
+      <div class="d-flex">
+        <v-icon color="blue lighten-1">mdi-baseball</v-icon>
+        <v-toolbar-title class="text-h4 font-weight-bold logo">高校野球</v-toolbar-title>
+      </div>
+      <router-link :to="{name: 'notification'}">
+        <v-icon color="blue lighten-1">mdi-bell</v-icon>
+        通知
+      </router-link>
+      <router-link :to="{name: 'user-show',params: {id: current_user}}">
+        <v-icon color="blue lighten-1">mdi-account</v-icon>
+        マイページ
+      </router-link>
+      <router-link :to="{name: 'contact'}">
+        <v-icon color="blue lighten-1">mdi-email</v-icon>
+        お問い合わせ
+      </router-link>
+    </v-app-bar>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      current_user: ''
+    }
+  },
+  mounted() {
+    this.fetchUser()
+  },
+  methods: {
+    fetchUser() {
+      axios
+        .get(`/api/v1/users.json`)
+        .then(response =>{
+          this.current_user = response.data[0].current_user
+        })
+    }
+  }
+}
+</script>
