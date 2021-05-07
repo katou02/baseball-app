c501790cda46faa1f029ceb421e5e081c46efda5
katou02 <tottoriitinose0202x@ezweb.ne.jp>
2021-05-07 20:50:14 +0900

    バリデーションメッセージ　スタイル変更

--- app/javascript/ForecastEditPage.vue
+++ app/javascript/ForecastEditPage.vue
@@ -49,7 +49,7 @@
         </div>
       </div>
       <v-textarea v-model="text" type="text" rows="2" cols="30" label="本文" outlined></v-textarea>
-      <p v-if="!!errors['text']" class="error" style="color: red;">{{ errors['text'][0]}}</p>
+      <p v-if="!!errors['text']" style="color: red;">{{ errors['text'][0]}}</p>
       <v-btn type="submit" color="primary" class="text-white mt-5">編集する</v-btn>
     </form>
     </v-container>
