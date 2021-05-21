c2f9dcfb59ccdc4818336a28cbfd2409825805a1
katou02 <56467807+katou02@users.noreply.github.com>
2021-05-21 15:33:13 +0900

    トップページ　デザイン

--- app/javascript/TopIndexPage.vue
+++ app/javascript/TopIndexPage.vue
@@ -1,9 +1,41 @@
 <template>
   <div class="top">
-    <Header></Header>
-    <router-link :to="{name: 'tweet'}">試合記事</router-link>
-    <router-link :to="{name: 'analysis'}">戦力分析</router-link>
-    <router-link :to="{name: 'forecast'}">試合予想</router-link>
+    <!-- <Header></Header> -->
+    <div class="top-title">
+      タイトルが入る
+    </div>
+    <div class="top-menu-list">
+      <router-link :to="{name: 'tweet'}" class="text-white">
+        <div class="top-menu">
+          <v-icon large color="white">mdi-pencil</v-icon><br>
+          試合記事
+        </div>
+      </router-link>
+      <router-link :to="{name: 'analysis'}" class="text-white">
+        <div class="top-menu">
+          <v-icon large color="white">mdi-magnify</v-icon><br>
+          戦力分析
+        </div>
+      </router-link>
+      <router-link :to="{name: 'forecast'}" class="text-white">
+        <div class="top-menu">
+          <v-icon large color="white">mdi-scale-balance</v-icon><br>
+          試合予想
+        </div>
+      </router-link>
+      <router-link :to="{name: 'champion',params: {id: 1}}" class="text-white">
+        <div class="top-menu">
+          <v-icon large color="white">mdi-crown</v-icon><br>
+          優勝予想
+        </div>
+      </router-link>
+      <router-link :to="{name: 'map',query: {tournament_id: 1}}" class="text-white">
+        <div class="top-menu">
+          <v-icon large color="white">mdi-city</v-icon><br>
+          ふるさと
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 <script>
