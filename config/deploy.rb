0498119cf7493402c3ec77b7b622cd25a83287a4
katou02 <56467807+katou02@users.noreply.github.com>
2021-05-22 14:48:43 +0900

    AWSキー

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/id_rsa_ef105ecb360761731c3987681ae06caa'] 
+                  keys: ['~/.ssh/baseball-app.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
