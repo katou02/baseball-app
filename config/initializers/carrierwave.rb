7e3829675e72287fb5399176a4fb9d59ea265b20
katou02 <56467807+katou02@users.noreply.github.com>
2021-05-23 11:50:28 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,15 +6,16 @@ CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
   else
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1'
-  }
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
+    }
 
-  config.fog_directory  = 'rails-baseball-app'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/rails-baseball-app'
+    config.fog_directory  = 'rails-baseball-app'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/rails-baseball-app'
   end
+end
