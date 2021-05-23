1f004b5f1818e037cf7c92095cfa49a29610ed3e
katou02 <56467807+katou02@users.noreply.github.com>
2021-05-23 23:44:05 +0900

    S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test?
-    config.storage = :file
-  else
+  # if Rails.env.development? || Rails.env.test?
+  #   config.storage = :file
+  # else
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -17,5 +17,5 @@ CarrierWave.configure do |config|
 
     config.fog_directory  = 'rails-baseball-app'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/rails-baseball-app'
-  end
+  # end
 end
