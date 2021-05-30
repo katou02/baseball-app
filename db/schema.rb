ff6d2eb04b307e9a605a74794f9e834338747e68
katou02 <56467807+katou02@users.noreply.github.com>
2021-05-30 11:32:34 +0900

    カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_25_050329) do
+ActiveRecord::Schema.define(version: 2021_05_30_021751) do
 
   create_table "analyses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
@@ -193,6 +193,7 @@ ActiveRecord::Schema.define(version: 2020_12_25_050329) do
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "PasswordDigest"
   end
 
   add_foreign_key "analyses", "categories", column: "school_id"
