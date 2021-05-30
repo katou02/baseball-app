79a38270c15d901ac9736f831aae71b7ca00bc0e
katou02 <56467807+katou02@users.noreply.github.com>
2021-05-30 11:06:39 +0900

    jwt_sessions準備

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,20 @@ class ApplicationController < ActionController::Base
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
+    include JWTSessions::RailsAuthorization
+    rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    end
+
+    private
+
+    # def current_user
+    #   @current_user ||= User.find(payload['user_id'])
+    # end
+  
+    def not_authorized
+      render json: { error: 'Not Authorized' }, status: :unauthorized
     end
 end
