cbec40e002728cf00476e54652d103b30f4e895b
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-04 00:37:38 +0900

    ログイン処理

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,13 +1,20 @@
 class ApiController < ApplicationController
-  before_action :authorize_access_request!,except: :index
+  # before_action :authorize_access_request!,except: :index
+  # before_action lambda{ cookie_based_auth(:access) }
   include JWTSessions::RailsAuthorization
   # rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
 
   private
 
   def current_user
-    # return unless request.headers.include? "Authorization"
-    @current_user ||= User.find(payload['user_id'])
+    if session_exists?(:access)
+      @current_user  ||= User.find(payload['user_id'])
+    else
+      cookie_based_auth(:access)
+      @current_user  ||= User.find(payload['user_id'])
+    end
+  rescue
+    nil
   end
   
   def not_authorized
