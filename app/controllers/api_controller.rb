a5bcfd37d4d663bc16edeb40578e6d9b8753761c
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-04 22:31:31 +0900

    修正

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,7 +2,7 @@ class ApiController < ApplicationController
   # before_action :authorize_access_request!,except: :index
   # before_action lambda{ cookie_based_auth(:access) }
   include JWTSessions::RailsAuthorization
-  # rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+  rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
 
   private
 
