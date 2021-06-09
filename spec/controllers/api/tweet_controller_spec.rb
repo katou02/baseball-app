ab062722a0884ebcb0de3cb4d708a06fee1b9d60
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-09 22:24:00 +0900

    test

---
+++ spec/controllers/api/tweet_controller_spec.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::TweetsController, type: :controller do
+  before do
+    @user = create(:user)
+    @tweet = create(:tweet,user_id: @user.id)
+  end
+  
+  describe 'index' do
+    context 'ログイン済み' do
+      it "正常なレスポンスを返す" do
+        sign_in @user
+        get :index,params: {id: @tweet.id}
+        expect(response).to be_success
+      end
+
+      it 'レスポンスコード200を返す' do
+        sign_in @user
+        get :index
+        expect(response).to have_http_status '200'
+      end
+    end
+  end
+
+  describe 'show' do
+    context 'ログイン済み' do
+      it "正常なレスポンスを返す" do
+        sign_in @user
+        get :show,params: {id: @tweet.id}
+        expect(response).to be_success
+      end
+
+      it 'レスポンコード200を返す' do
+        sign_in @user
+        get :show,params: {id: @tweet.id}
+        expect(response).to have_http_status '200'
+      end
+    end
+  end
+
+  describe 'new' do
+    context 'ログイン済み' do
+      it "正常なレスポンスを返す" do
+        sign_in @user
+        get :new
+        expect(response).to be_success
+      end
+
+      it 'レスポンスコード200を返す' do
+        sign_in @user
+        get :new,params: {id: @tweet.id}
+        expect(response).to have_http_status '200'
+      end
+    end
+  end
+
+  describe 'edit' do
+    context 'ログイン済み' do
+      it "正常なレスポンスを返す" do
+        sign_in @user
+        get :edit,params: {id: @tweet.id}
+        expect(response).to be_success
+      end
+
+      it 'レスポンスコード200を返す' do
+        sign_in @user
+        get :edit,params: {id: @tweet.id}
+        expect(response).to have_http_status '200'
+      end
+    end
+  end
+
+  describe 'create' do
+    context 'ログイン済み' do
+      it '保存できること' do
+        sign_in @user
+        expect {
+          post :create,params: @tweet
+          expect(response).to
+        }
+      end
+    end
+  end
+
+  describe 'update' do
+    context 'ログイン済み' do
+      it '更新できること' do
+        sign_in @user
+        tweet_params = {text: '更新'}
+        patch :update,params: {id: @tweet.user_id,tweet: tweet_params}
+      end
+    end
+  end
+end
