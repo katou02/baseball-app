431b1b0a63cdc0001fce547ed6dc2556c891466c
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-10 19:57:07 +0900

    修正

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :controller do
   before do
     @user = create(:user)
-    allow(controller)
-    .to receive(:current_user)
-    .and_return(@user)
+      allow(controller)
+      .to receive(:current_user)
+      .and_return(@user)
   end
   
   describe 'index' do
@@ -24,13 +24,13 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     context 'ログイン済み' do
       it "正常なレスポンスを返す" do
         sign_in @user
-        get :show,params: {id: 1}
+        get :show,params: {id: @user.id}
         expect(response).to be_success
       end
 
       it 'レスポンコード200を返す' do
         sign_in @user
-        get :show,params: {id: 1}
+        get :show,params: {id: @user.id}
         expect(response).to have_http_status '200'
       end
     end
