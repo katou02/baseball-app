58e15989f1e596c61a8ae674a3e7d79450e7139d
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-10 13:16:18 +0900

    Rspec テスト

--- spec/factories/champions.rb
+++ spec/factories/champions.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :champion do
+    id {1}
     association :champion_school,factory: :category
     association :tournament,factory: :tournament
     association :user,factory: :user
