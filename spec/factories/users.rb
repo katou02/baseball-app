6700c75be7ff709bd078a52b05cc8eea112d3d32
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-10 15:43:20 +0900

    Rspec テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :user,class: User do
     id { 20 }
     nickname { "test" }
+    text {"自己紹介"}
     email { "test1@gmail.com" }
     password { "test2020" }
     password_confirmation { "test2020" }
