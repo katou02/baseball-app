23301842465ba0a63ffc5ccfbdbb6b2a95b1b2cf
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-12 21:24:52 +0900

    Update tweets.rb

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     school_a_score { 1 }
     school_b_score { 2 }
     text {"テキスト"}
+    round {"1回戦"}
     association :school_a,factory: :category
     association :school_b,factory: :category_2
     association :tournament,factory: :tournament
