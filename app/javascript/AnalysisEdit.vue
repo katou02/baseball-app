c0a8adb0dee07b83681915fd6ab90e17b55758ba
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-14 23:41:24 +0900

    編集ページ　条件

--- app/javascript/AnalysisEdit.vue
+++ app/javascript/AnalysisEdit.vue
@@ -140,7 +140,7 @@ export default {
   },
   beforeUpdate() {
     if(!this.current_user || this.current_user!==this.user_id) {
-      this.$router.push({ name: 'analysis'});
+      this.$router.push({ name: 'analysis-show'});
     }
   },
   methods: {
