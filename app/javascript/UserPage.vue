7f712f884fb5db3c2ee7037c5a8b578d51a1317e
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-15 15:58:34 +0900

    Update UserPage.vue

--- app/javascript/UserPage.vue
+++ app/javascript/UserPage.vue
@@ -78,6 +78,11 @@ export default {
         .get('api/v1/users.json')
         .then(response =>{
           this.users = response.data.users;
+          if(!response.data.current_user) {
+            delete localStorage.csrf
+            delete localStorage.signedIn
+            this.$router.push({ name: 'top'});
+          }
         })
     },
     clickCallback: function (pageNum) {
