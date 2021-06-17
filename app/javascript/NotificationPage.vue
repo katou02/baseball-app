bb41fb2415789c8802bba95465089fb98ae0f278
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-17 23:33:10 +0900

    自分の通知

--- app/javascript/NotificationPage.vue
+++ app/javascript/NotificationPage.vue
@@ -8,38 +8,38 @@
     <div class="notification">
       <div class="mx-auto text-center">
         <div v-for="e in listNotifications" :key="e.id">
-          <div v-if="e.visiter_id!=e.visited_id">
-            <div class="notification-info mt-5">
-              <div v-if="e.visiter_image.url"> 
-                <img :src= e.visiter_image.url class="user-icon mt-1 mb-5">
-              </div>
-              <div v-else>
-                <img src="../assets/images/no-image.png" class="user-icon mt-1 mb-5">
-              </div>
-              <div class="mt-5 ml-5">
-              <router-link :to="{name: 'user-show',params: {id: e.visiter_id}}">{{e.visiter}}</router-link>が
-              <span v-if="e.action==='follow'">
-                あなたをフォローしました
-              </span>
-              <span v-else-if="e.action==='like'">
-                あなたの<router-link :to="{name: 'tweet-show',params: {id: e.tweet}}">投稿</router-link>にいいねをしました
-              </span>
-              <span v-else-if="e.action==='comment'">
-                あなたの<router-link :to="{name: 'tweet-show',params: {id: e.tweet}}">投稿</router-link>にコメントをしました
-              </span>
-              <span v-else-if="e.action==='comment_analysis'">
-                あなたの<router-link :to="{name: 'analysis-show',params: {id: e.analysis}}">投稿</router-link>にコメントをしました
-              </span>
-              <span v-else-if="e.action==='comment_forecast'">
-                あなたの<router-link :to="{name: 'forecast-show',params: {id: e.forecast}}">投稿</router-link>にコメントをしました
-              </span>
-              <span v-else-if="e.action==='dm'">
-                あなたに<router-link :to="{name: 'room-show',params: {id: e.room}}">メッセージ</router-link>を送りました
-              </span>
-              ({{e.time+"前"}})
-              </div>
+          <!-- <div v-if="e.visiter_id!=e.visited_id"> -->
+          <div class="notification-info mt-5">
+            <div v-if="e.visiter_image.url"> 
+              <img :src= e.visiter_image.url class="user-icon mt-1 mb-5">
+            </div>
+            <div v-else>
+              <img src="../assets/images/no-image.png" class="user-icon mt-1 mb-5">
+            </div>
+            <div class="mt-5 ml-5">
+            <router-link :to="{name: 'user-show',params: {id: e.visiter_id}}">{{e.visiter}}</router-link>が
+            <span v-if="e.action==='follow'">
+              あなたをフォローしました
+            </span>
+            <span v-else-if="e.action==='like'">
+              あなたの<router-link :to="{name: 'tweet-show',params: {id: e.tweet}}">投稿</router-link>にいいねをしました
+            </span>
+            <span v-else-if="e.action==='comment'">
+              あなたの<router-link :to="{name: 'tweet-show',params: {id: e.tweet}}">投稿</router-link>にコメントをしました
+            </span>
+            <span v-else-if="e.action==='comment_analysis'">
+              あなたの<router-link :to="{name: 'analysis-show',params: {id: e.analysis}}">投稿</router-link>にコメントをしました
+            </span>
+            <span v-else-if="e.action==='comment_forecast'">
+              あなたの<router-link :to="{name: 'forecast-show',params: {id: e.forecast}}">投稿</router-link>にコメントをしました
+            </span>
+            <span v-else-if="e.action==='dm'">
+              あなたに<router-link :to="{name: 'room-show',params: {id: e.room}}">メッセージ</router-link>を送りました
+            </span>
+            ({{e.time+"前"}})
             </div>
           </div>
+          <!-- </div> -->
         </div>
         <button
           class="list-item-button text-center"
