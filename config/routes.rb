854ddcdef0162fad799b3bbf1c7a7354d65b4eae
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-19 19:03:06 +0900

    不要削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in',to: 'users/sessions#new_guest'
   end
-  # resources :tweets do
-  #   # collection do
-  #   #   get :search
-  #   # end
-  #   resources :comments,only:[:create,:destroy]
-  #   resources :likes,only:[:create,:destroy]
-  # end
   
   namespace :api,{format: 'json'} do
     namespace :v1 do
@@ -56,50 +49,6 @@ Rails.application.routes.draw do
           post :check
         end
       end
-    end
-  end
-  
-  resources :tops,only:[:index]
-  # resources :tournaments,only:[:show] do
-  #   member do
-  #     get :watch_ays,:watch_avg,:watch_fcs
-  #   end
-  # end
-  # resources :analyses do
-  #   collection do
-  #     get :search
-  #   end
-  #   resources :comment_analyses,only:[:create,:destroy]
-  # end
-  # resources :forecasts do
-  #   collection do
-  #     get :search
-  #   end
-  #   resources :comment_forecasts,only:[:create,:destroy]
-  # end
-  namespace :admin do
-    resources :users,only: [:index,:destroy] do
-      collection do
-        get :users_show
-      end
-    end
-  end
-  # resources :users,only:[:index,:show,:edit,:update] do
-  #   member do
-  #     get :following,:followers
-  #     get :likes
-  #   end
-  # end
-  # resources :relationships, only: [:create,:destroy]
-  # resources :notifications, only: :index
-  # resources :messages, only: :create
-  # resources :rooms,only: [:index,:create,:show]
-  # resources :champions,only: [:new,:create,:destroy,:show]
-  # resources :maps,only: [:index,:new,:create,:edit,:update,:show,:destroy]
-  resources :contacts,only:[:index] do
-    collection do
-      post :check
-      post :done
     end
   end
   get '*path', to: 'tops#index'
