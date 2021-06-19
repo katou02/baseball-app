5cb59a5d7109dd4d81e966bce154c6963a7d7a55
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-19 19:34:42 +0900

    リファクタリング

--- app/controllers/api/v1/champions_controller.rb
+++ app/controllers/api/v1/champions_controller.rb
@@ -22,7 +22,9 @@ class Api::V1::ChampionsController < ApiController
     date.push(@school)
 
     date[0].size.times do |n|
+      # 学校
       @schools<<date[0][n].champion_school.name
+      # 票数
       @vote<<@num[0][n][1]
     end
     render 'show', formats: 'json', handlers: 'jbuilder'
@@ -39,26 +41,26 @@ class Api::V1::ChampionsController < ApiController
 
   def ranking(champions)
     @num = []
+    #集計する
     champion_count = champions.group(:champion_school_id).count
     # 重複が多い順に並び替え
     @num << champion_count.sort {|(k1, v1), (k2, v2)| v2 <=> v1 }
-    # @num[0].slice!(5,70)
-    # @num[0][n][1] 1つめのn学校,2つめは票数
+    # @num = [[[n,m],[n,m]]]
+    # nが学校 mが票数
     @school = []
     # 多い順に格納
     @num[0].length.times do |n|
+      # 学校を入れている
       @school << Champion.find_by(champion_school_id: @num[0][n][0])
     end
   end
 
   def destroy
     @my_champion = Champion.find_by(user_id: current_user.id,tournament_id: params[:id])
-    if @my_champion.user_id == current_user.id
-      if @my_champion.destroy
-        head :no_content
-      else
-        render json: @my_champion.errors, status: :unprocessable_entity
-      end
+    if (@my_champion.user_id == current_user.id) && @my_champion.destroy
+      head :no_content
+    else
+      render json: @my_champion.errors, status: :unprocessable_entity
     end
   end
 
