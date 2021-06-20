510d68f5e6f7e3d3a986f12fbf8f3e6223a8cea8
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-20 09:45:43 +0900

    フロント修正

--- app/javascript/ForecastPage.vue
+++ app/javascript/ForecastPage.vue
@@ -30,7 +30,7 @@
         <Side></Side>
         <v-row class="ml-5">
           <v-col cols="12"  sm="12" md="12" lg="6" v-for="e in getLists" :key="e.id">
-            <div class="forecast mt-5">
+            <div class="article mt-5">
               <router-link :to="{name: 'forecast-show',params: {id: e.id}}">
                 <div class="d-flex h-100">
                   <div class="forecast-image"><img src="/images/ball.jpg" class="article-icon"></div>
