916a3ceab8e9fcf58f335354b662749edfe9b20a
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-24 01:00:20 +0900

    タイトル

--- app/javascript/TopPage.vue
+++ app/javascript/TopPage.vue
@@ -7,31 +7,31 @@
     <div class="top-menu-list">
       <router-link :to="{name: 'tweet'}" class="text-white">
         <div class="top-menu">
-          <v-icon large color="white">mdi-pencil</v-icon><br>
+          <v-icon large color="blue">mdi-pencil</v-icon><br>
           試合記事
         </div>
       </router-link>
       <router-link :to="{name: 'analysis'}" class="text-white">
         <div class="top-menu">
-          <v-icon large color="white">mdi-magnify</v-icon><br>
+          <v-icon large color="blue">mdi-magnify</v-icon><br>
           戦力分析
         </div>
       </router-link>
       <router-link :to="{name: 'forecast'}" class="text-white">
         <div class="top-menu">
-          <v-icon large color="white">mdi-scale-balance</v-icon><br>
+          <v-icon large color="blue">mdi-scale-balance</v-icon><br>
           試合予想
         </div>
       </router-link>
       <router-link :to="{name: 'champion',params: {id: 1}}" class="text-white">
         <div class="top-menu">
-          <v-icon large color="white">mdi-crown</v-icon><br>
+          <v-icon large color="orange">mdi-crown</v-icon><br>
           優勝予想
         </div>
       </router-link>
       <router-link :to="{name: 'map',query: {tournament_id: 1}}" class="text-white">
         <div class="top-menu">
-          <v-icon large color="white">mdi-city</v-icon><br>
+          <v-icon large color="blue">mdi-city</v-icon><br>
           ふるさと
         </div>
       </router-link>
