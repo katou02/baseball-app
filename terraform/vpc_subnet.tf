70635916c61126f714a75ef63738bc3164450247
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-25 23:29:37 +0900

    Subnet構築

---
+++ terraform/vpc_subnet.tf
@@ -0,0 +1,19 @@
+resource "aws_subnet" "public-a" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "pub-a"
+  }
+}
+
+resource "aws_subnet" "private-a" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "priv-a"
+  }
+}
