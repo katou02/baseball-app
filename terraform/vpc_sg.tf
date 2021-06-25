4311fb52c59b3336ad5cefd216381ac546e3f57c
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-26 00:06:41 +0900

    セキュリティグループ

---
+++ terraform/vpc_sg.tf
@@ -0,0 +1,61 @@
+resource "aws_security_group" "pub-a" {
+  name = "sg_pub-a"
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "sg_pub-a"
+  }
+}
+
+resource "aws_security_group_rule" "sg_egress_pub-a" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.pub-a.id
+}
+
+resource "aws_security_group_rule" "ingress_pub-a_22" {
+  type = "ingress"
+  from_port = "22"
+  to_port = "22"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.pub-a.id
+}
+
+resource "aws_security_group_rule" "ingress-pub-a-80" {
+  type = "ingress"
+  from_port = "88"
+  to_port = "88"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.pub-a.id
+}
+
+resource "aws_security_group" "priv-a" {
+  name   = "sg_priv-a"
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "sg_priv-a"
+  }
+}
+
+resource "aws_security_group_rule" "egress-priv-a" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.priv-a.id
+}
+
+resource "aws_security_group_rule" "ingress-priv-a-22" {
+  type = "ingress"
+  from_port = "22"
+  to_port = "22"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.priv-a.id
+}
