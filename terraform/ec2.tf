1c6abc716116259ba66b77138cfb4eb710aae90a
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-26 00:37:52 +0900

    APサーバ

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -18,3 +18,27 @@ user_data = <<EOF
   systemctl start nginx.service
 EOF
 }
+
+resource "aws_instance" "ap" {
+  ami           = "${data.aws_ami.amzn2.id}"
+  instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.auth_priv.id}"
+    subnet_id = "${aws_subnet.private-a.id}"
+  vpc_security_group_ids = ["${aws_security_group.priv-a.id}"]
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 8
+    delete_on_termination = true
+  }
+  tags = {
+    Name = "ap-instance"
+  }
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y https://dev.mysql.com/get/mysql80-community-release-el7-3.
+    noarch.rpm
+    yum-config-manager --disable mysql80-community
+    yum-config-manager --enable mysql57-community
+    yum install -y mysql-community-client
+  EOF 
+}
