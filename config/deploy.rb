c5386bda88b76ec25f67bdeab4202a45e87f85ac
katou02 <56467807+katou02@users.noreply.github.com>
2021-06-26 16:07:29 +0900

    変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,8 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/baseball-app.pem'] 
+                  # keys: ['~/.ssh/baseball-app.pem']
+                  keys: ['~/.ssh/kousien_key'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
-server '18.180.246.163', user: 'ec2-user', roles: %w{app db web}
+# server '18.180.246.163', user: 'ec2-user', roles: %w{app db web}
+server '54.95.179.208', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
