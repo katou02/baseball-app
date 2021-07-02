a42e1ab3a0473443c8894fd98170f63b329b977b
katou02 <56467807+katou02@users.noreply.github.com>
2021-07-02 23:10:09 +0900

    制約

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :tweet
   belongs_to :user
+  has_many :notifications,dependent: :destroy
   validates :text,presence: :true
   validates :text,length: {maximum: 300}
 end
