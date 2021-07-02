077a53fb9b6033b1234f0d93b8a563d68707fda4
katou02 <56467807+katou02@users.noreply.github.com>
2021-07-03 00:47:18 +0900

    投稿モーダル

--- app/javascript/WatchtwsPage.vue
+++ app/javascript/WatchtwsPage.vue
@@ -3,7 +3,8 @@
     <!-- <Header></Header> -->
     <div class="main-content-btn">
       <div v-if="current_user">
-        <router-link :to="{name: 'tweet-new'}" class="send-btn text-white">投稿する</router-link>
+        <!-- <router-link :to="{name: 'tweet-new'}" class="send-btn text-white">投稿する</router-link> -->
+        <button class="send-btn text-white" @click="openModal">投稿する</button>
       </div>
       <router-link :to="{name: 'top'}" class="return-top text-white">トップページ</router-link>
     </div>
@@ -56,6 +57,12 @@
           </div>
           </v-row>
         </div>
+        <modal name="select" height="auto" width="65%" :scrollable="true">
+          <div id="modal">
+            <New @parent-event="fetchTweets"></New>
+            <button @click="closeModal">閉じる</button>
+          </div>
+        </modal>
         <div class="text-center">
           <paginate
             :v-model="currentPage" 
@@ -81,10 +88,12 @@
 import axios from 'axios'
 import Header from './components/Header.vue'
 import Side from './components/Side.vue'
+import New from './components/TweetNew.vue'
 export default {
   components: {
     Header,
-    Side
+    Side,
+    New
   },
   data() {
     return {
@@ -109,6 +118,7 @@ export default {
   },
   methods: {
     fetchTweets() {
+      this.closeModal()
       axios
         .get(`/api/v1/tournaments/${this.$route.params.id}.json`)
         .then(response =>{
@@ -141,7 +151,13 @@ export default {
           }, 500);
       })
       // this.fetchTweets()
-    }
+    },
+    openModal(){
+      this.$modal.show('select');
+    },
+    closeModal(){
+      this.$modal.hide('select');
+    },
   },
   computed: {
     getTweets: function() {
