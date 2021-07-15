fa8b63760938ca8323eeee56ec8fa49353259b74
katou02 <56467807+katou02@users.noreply.github.com>
2021-07-16 00:01:49 +0900

    mt5

--- app/javascript/WatchtwsPage.vue
+++ app/javascript/WatchtwsPage.vue
@@ -28,7 +28,7 @@
         </div>
         <Img></Img>
         <!-- 記事 -->
-        <div class="d-md-flex list">
+        <div class="d-md-flex list mt-5">
           <Side></Side>
           <v-row class="ml-5">
             <v-col cols="12"  sm="12" md="12" lg="6" v-for="e in getLists" :key="e.id">
