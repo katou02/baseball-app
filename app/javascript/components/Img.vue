5f583c2c617e4d6c4fbc2f4875d6beb363c30501
katou02 <56467807+katou02@users.noreply.github.com>
2021-07-16 00:01:17 +0900

    画像追加

--- app/javascript/components/Img.vue
+++ app/javascript/components/Img.vue
@@ -4,5 +4,6 @@
     <img src="/assets/pitcher.jpeg" class="img2" height="200" width="200">
     <img src="/assets/batter2.jpeg" class="img3" height="200" width="200">
     <img src="/assets/defensive.jpeg" class="img4" height="200" width="200">
+    <img src="/assets/batter3.jpeg" class="img5" height="200" width="200">
   </div>
 </template>
